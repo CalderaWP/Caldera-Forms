c2cb72f9161ccbbb4ff73b4916dced1f0750f516
David Cramer <david@digilab.co.za>
2014-05-18 16:04:22 +0200

    picker field

---
+++ fields/color_picker/field.php
@@ -0,0 +1,9 @@
+<div class="<?php echo $field_wrapper_class; ?> cf-color-picker">
+	<?php echo $field_label; ?>
+	<div class="row">
+		<div class="<?php echo $field_input_class; ?> col-sm-5 col-md-4 input-group">
+			<input <?php echo $field_placeholder; ?> id="<?php echo $field_id; ?>" type="text" class="<?php echo $field_class; ?> minicolor-picker init_field_type" data-type="color_picker" name="<?php echo $field_name; ?>" value="<?php echo $field_value; ?>">		
+		</div>
+	</div>
+	<?php echo $field_caption; ?>
+</div>
