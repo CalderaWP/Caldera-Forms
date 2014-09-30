3fc27170a4472b0bac96ecf86598a894ce9602c8
David Cramer <david@digilab.co.za>
2014-09-30 09:03:46 +0200

    spacing in attributes

--- fields/text/field.php
+++ fields/text/field.php
@@ -11,7 +11,7 @@ if(!empty($field['config']['masked'])){
 ?><div class="<?php echo $field_wrapper_class; ?>">
 	<?php echo $field_label; ?>
 	<div class="<?php echo $field_input_class; ?>">
-		<input <?php echo $field_placeholder; ?> type="text"<?php echo $mask; ?> data-field="<?php echo $field_base_id; ?>" class="<?php echo $field_class; ?>" id="<?php echo $field_id; ?>" name="<?php echo $field_name; ?>" value="<?php echo htmlentities( $field_value ); ?>" <?php echo $field_required; ?>>
+		<input <?php echo $field_placeholder; ?> type="text" <?php echo $mask; ?> data-field="<?php echo $field_base_id; ?>" class="<?php echo $field_class; ?>" id="<?php echo $field_id; ?>" name="<?php echo $field_name; ?>" value="<?php echo htmlentities( $field_value ); ?>" <?php echo $field_required; ?>>
 		<?php echo $field_caption; ?>
 	</div>
 </div>
