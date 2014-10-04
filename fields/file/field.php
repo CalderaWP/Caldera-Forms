390fcd874f9959754d2a09c3d846eb5b022c1c25
David Cramer <david@digilab.co.za>
2014-10-04 18:19:04 +0200

    hide overflow text in file

--- fields/file/field.php
+++ fields/file/field.php
@@ -1,6 +1,6 @@
 <div class="<?php echo $field_wrapper_class; ?>">
 	<?php echo $field_label; ?>
-	<div class="<?php echo $field_input_class; ?>">
+	<div class="<?php echo $field_input_class; ?>" style="overflow: hidden;">
 		<input <?php echo $field_placeholder; ?> type="file" data-field="<?php echo $field_base_id; ?>" id="<?php echo $field_id; ?>" name="<?php echo $field_name; ?>" <?php echo $field_required; ?>>
 		<input type="hidden" name="<?php echo $field_name; ?>" vale="true">
 		<?php echo $field_caption; ?>
