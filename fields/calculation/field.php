b38fd11c988ebf2bac99cc14f1f87b414142ae0c
David Cramer <david@digilab.co.za>
2015-04-09 12:00:00 +0200

    added money format to display

--- fields/calculation/field.php
+++ fields/calculation/field.php
@@ -66,6 +66,7 @@ if(!empty($binds)){
 ?>
 <script type="text/javascript">
 	jQuery(function($){
+
 		function checked_total_<?php echo $field_base_id; ?>(items){
 			var sum = 0;
 			items.each(function(k,v){
@@ -75,14 +76,27 @@ if(!empty($binds)){
 		}
 		function docalc_<?php echo $field_base_id; ?>(){
 			var <?php echo implode(', ',$binds_vars); ?>,
-				total = <?php echo $formula; ?>;
+				total = <?php echo $formula; ?>,
+				view_total = total;
 
 
 			<?php if(!empty($field['config']['fixed'])){ ?>
+			function addCommas(nStr){
+				nStr += '';
+				x = nStr.split('.');
+				x1 = x[0];
+				x2 = x.length > 1 ? '.' + x[1] : '';
+				var rgx = /(\d+)(\d{3})/;
+				while (rgx.test(x1)) {
+					x1 = x1.replace(rgx, '$1' + ',' + '$2');
+				}
+				return x1 + x2;
+			}
 			total = total.toFixed(2);
+			view_total = addCommas( total );
 			<?php } ?>
 
-			$('#<?php echo $field_id; ?>').html( total );
+			$('#<?php echo $field_id; ?>').html( view_total );
 			$('[data-field="<?php echo $field_base_id; ?>"]').val( total ).trigger('change');
 
 		}
