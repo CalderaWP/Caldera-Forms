bbff4f60df0dd76926d48f13fe5d0e4cd49fb6c5
Josh Pollock <jpollock412@gmail.com>
2015-05-01 02:17:09 -0400

    Ensure $field_value is a string in hidden field
    fixes #133

--- fields/hidden/field.php
+++ fields/hidden/field.php
@@ -1,1 +1,11 @@
+<?php
+if ( is_array( $field_value ) )  {
+	if ( isset( $field_value[0] ) ) {
+		$field_value = $field_value[0];
+	}else{
+		$field_value = ' ';
+	}
+
+}
+?>
 <input type="hidden" id="<?php echo $field_id; ?>" data-field="<?php echo $field_base_id; ?>" name="<?php echo $field_name; ?>" value="<?php echo htmlentities( $field_value ); ?>">
