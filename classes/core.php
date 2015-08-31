fa224aefece8e67f08d4f36116d31134510c11b8
David Cramer <david@digilab.co.za>
2015-08-31 21:30:03 +0200

    init system a little later.

--- classes/core.php
+++ classes/core.php
@@ -68,7 +68,7 @@ class Caldera_Forms {
 
 		add_action("wp_ajax_get_entry", array( $this, 'get_entry') );
 		// find if profile is loaded
-		add_action('init', array( $this, 'cf_init_system'));
+		add_action('init', array( $this, 'cf_init_system'), 25 );
 		add_action('wp', array( $this, 'cf_init_preview'));
 
 		// render shortcode
