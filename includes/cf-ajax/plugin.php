51aa8c5aa6a70bda37727f8e7edd05a3656eff39
David Cramer <david@digilab.co.za>
2015-09-28 07:49:30 +0200

    cf api endpoint

--- includes/cf-ajax/plugin.php
+++ includes/cf-ajax/plugin.php
@@ -271,7 +271,7 @@ function cf_ajax_setatts($atts, $form){
 		'data-load-class' 	=>	'cf_processing',
 		'data-post-disable' =>	$post_disable,
 		'data-action'		=>	'cf_process_ajax_submit',
-		'data-request'		=>	admin_url( 'admin-ajax.php' ),		
+		'data-request'		=>	home_url( '/api/cf/' . $form['ID'] ),		
 	);
 	
 	if( !empty( $form['custom_callback'] ) ){
