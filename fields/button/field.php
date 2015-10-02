96117198afe7d6f5e696bc11607c94b29709cd95
David Cramer <david@digilab.co.za>
2015-10-02 07:51:11 +0200

    no autocomplete tag on hidden

--- fields/button/field.php
+++ fields/button/field.php
@@ -18,7 +18,7 @@ if($field['config']['type'] == 'next' || $field['config']['type'] == 'prev'){
 
 
 ?><?php echo $wrapper_before; ?><?php echo $field_before; ?><input data-field="<?php echo $field_base_id; ?>_btn" <?php echo $btn_action; ?> class="<?php echo $field['config']['class']; ?>" type="<?php echo $btnType; ?>" name="<?php echo $field_name; ?>_btn" value="<?php echo esc_attr( $field['label'] ); ?>" id="<?php echo $field_id; ?>"><?php echo $field_after; ?><?php echo $wrapper_after; ?>
-<input class="button_trigger_<?php echo $current_form_count; ?>" type="hidden" data-field="<?php echo $field_base_id; ?>" id="<?php echo $field_id; ?>_btn" name="<?php echo $field_name; ?>" value="<?php echo esc_attr( $field_value ); ?>" autocomplete="off">
+<input class="button_trigger_<?php echo $current_form_count; ?>" type="hidden" data-field="<?php echo $field_base_id; ?>" id="<?php echo $field_id; ?>_btn" name="<?php echo $field_name; ?>" value="<?php echo esc_attr( $field_value ); ?>">
 <?php
 ob_start();
 ?>
