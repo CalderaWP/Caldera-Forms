f2dea986fc89d2f9214e4ecfca3d51903881cbdc
David Cramer <david@digilab.co.za>
2015-10-02 08:43:40 +0200

    fixes #261

--- assets/js/frontend-script-init.js
+++ assets/js/frontend-script-init.js
@@ -3,15 +3,15 @@ var cf_jsfields_init;
 
 	// make init function
 	cf_jsfields_init = function(){
-		jQuery('.init_field_type[data-type]').each(function(k,v){
-			var ftype = jQuery(v);
+		$('.init_field_type[data-type]').each(function(k,v){
+			var ftype = $(v);
 			if( typeof window[ftype.data('type') + '_init'] === 'function' ){
 				window[ftype.data('type') + '_init'](ftype.prop('id'), ftype[0]);
 			}
 		});
 	};	
 
-	jQuery('document').ready(function(){
+	$('document').ready(function(){
 		// check for init function
 		cf_jsfields_init();
 
@@ -19,52 +19,61 @@ var cf_jsfields_init;
     		modules : 'html5'
 		});
 	});
+
+	// if pages, disable enter
+	if( $('.caldera-form-page').length ){
+		$('.caldera-form-page').on('keypress', '[data-field]:not(textarea)', function( e ){
+			if( e.keyCode === 13 ){
+				e.preventDefault();
+			}
+		});
+	}
 	// modals activation
-	jQuery(document).on('click', '.cf_modal_button', function(e){
+	$(document).on('click', '.cf_modal_button', function(e){
 		e.preventDefault();
-		var clicked = jQuery(this);
-		jQuery(clicked.attr('href')).show();
+		var clicked = $(this);
+		$(clicked.attr('href')).show();
 	});
-	jQuery(document).on('click', '.caldera-front-modal-closer', function(e){
+	$(document).on('click', '.caldera-front-modal-closer', function(e){
 		e.preventDefault();
-		var clicked = jQuery(this);
+		var clicked = $(this);
 			clicked.closest('.caldera-front-modal-container').hide();
 	});
 	// stuff trigger
-	jQuery(document).on('cf.add cf.enable cf.disable cf.pagenav', cf_jsfields_init );
+	$(document).on('cf.add cf.enable cf.disable cf.pagenav', cf_jsfields_init );
 	
 	// Page navigation
-	jQuery(document).on('click', '[data-page]', function(e){
+	$(document).on('click', '[data-page]', function(e){
 
-		var clicked = jQuery(this),
+		var clicked = $(this),
 			page_box = clicked.closest('.caldera-form-page'),
 			form 	 = clicked.closest('.caldera-grid'),
 			page	 = page_box.data('formpage'),
-			breadcrumb = jQuery('.breadcrumb[data-form="' + form.prop('id') + '"]'),
+			breadcrumb = $('.breadcrumb[data-form="' + form.prop('id') + '"]'),
 			next,
 			prev,
 			fields,
-			checks = {};			
+			checks = {};
 		if( !form.length ){
 			return;
 		}
 
 		// pre validate
 		if(clicked.data('pagenav')){
-			fields = jQuery('#' + clicked.data('pagenav') + ' .caldera-form-page').find('[data-field]');
+			fields = $('#' + clicked.data('pagenav') + ' .caldera-form-page').find('[data-field]');
 			form = clicked.closest('.caldera_forms_form');
 		}else{
 			fields = form.find('.caldera-form-page:visible').find('[data-field]');				
 		}
 		for(var f = 0; f < fields.length; f++){
-			if( jQuery(fields[f]).is(':radio,:checkbox') ){
+			if( $(fields[f]).is(':radio,:checkbox') ){
 				if( !$(fields[f]).hasClass('option-required') ){continue}
 				if( !checks[$(fields[f]).data('field')] ){
 					checks[$(fields[f]).data('field')] = [];
 				}
 				checks[$(fields[f]).data('field')].push($(fields[f]).prop('checked'));
 			}else{
-				if(jQuery(fields[f]).prop('required')){
+				if($(fields[f]).prop('required')){
 					if(!fields[f].value.length){
 						e.preventDefault();
 						//nope submit form to indicate a fail.
@@ -122,26 +131,26 @@ var cf_jsfields_init;
 			if(clicked.data('pagenav')){
 				e.preventDefault();
 				clicked.closest('.breadcrumb').find('li.active').removeClass('active');
-				jQuery('#' + clicked.data('pagenav') + ' .caldera-form-page').hide();
-				jQuery('#' + clicked.data('pagenav') + '	.caldera-form-page[data-formpage="'+ ( clicked.data('page') ) +'"]').show();
+				$('#' + clicked.data('pagenav') + ' .caldera-form-page').hide();
+				$('#' + clicked.data('pagenav') + '	.caldera-form-page[data-formpage="'+ ( clicked.data('page') ) +'"]').show();
 				clicked.parent().addClass('active');
 			}
 			
 		}
-		jQuery('html, body').animate({
+		$('html, body').animate({
 			scrollTop: form.offset().top - 100
 		}, 200);
 		
-		jQuery(document).trigger('cf.pagenav');
+		$(document).trigger('cf.pagenav');
 
 	})
 	// init page errors
 	var tab_navclick;
-	jQuery('.caldera-grid .breadcrumb').each(function(k,v){
-		jQuery(v).find('a[data-pagenav]').each(function(i,e){
-			var tab		= jQuery(e),
+	$('.caldera-grid .breadcrumb').each(function(k,v){
+		$(v).find('a[data-pagenav]').each(function(i,e){
+			var tab		= $(e),
 				form 	= tab.data('pagenav'),
-				page	= jQuery('#'+ form +' .caldera-form-page[data-formpage="' + tab.data('page') + '"]');
+				page	= $('#'+ form +' .caldera-form-page[data-formpage="' + tab.data('page') + '"]');
 
 			if(page.find('.has-error').length){
 				tab.parent().addClass('error');
