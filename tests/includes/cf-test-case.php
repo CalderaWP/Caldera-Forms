cd87d1882200c87e53e23e719269dfdccb134edf
Josh Pollock <jpollock412@gmail.com>
2016-03-15 18:04:39 -0400

    Setup phpunit #468

---
+++ tests/includes/cf-test-case.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Base test case class
+ *
+ * @package   Caldera_Forms
+ * @author    Josh Pollock <Josh@JoshPress.net>
+ * @license   GPL-2.0+
+ * @link
+ * @copyright 2016 Josh Pollock
+ */
+abstract class Caldera_Forms_Test_Case extends WP_UnitTestCase {
+
+
+}
