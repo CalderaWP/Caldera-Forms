30e98d950470991e67665d576ce714f64b87777a
Josh Pollock <jpollock412@gmail.com>
2016-03-29 21:04:49 -0400

    Actually echo the fucking labels... #501

--- processors/auto_responder/config.php
+++ processors/auto_responder/config.php
@@ -1,20 +1,20 @@
 <div class="caldera-config-group">
 	<label>
-		<?php esc_html__('From Name', 'caldera-forms'); ?>
+		<?php esc_html_e('From Name', 'caldera-forms'); ?>
 	</label>
 	<div class="caldera-config-field">
 		<input type="text" class="block-input field-config magic-tag-enabled required" name="{{_name}}[sender_name]" value="{{sender_name}}">
 	</div>
 </div>
 <div class="caldera-config-group">
-	<label><?php esc_html__('From Email', 'caldera-forms'); ?> </label>
+	<label><?php esc_html_e('From Email', 'caldera-forms'); ?> </label>
 	<div class="caldera-config-field">
 		<input type="text" class="block-input field-config magic-tag-enabled caldera-field-bind required" id="{{_id}}_sender_email" name="{{_name}}[sender_email]" value="{{sender_email}}">
 	</div>
 </div>
 <div class="caldera-config-group">
 	<label>
-		<?php esc_html__('Email Subject', 'caldera-forms'); ?>
+		<?php esc_html_e('Email Subject', 'caldera-forms'); ?>
 	</label>
 	<div class="caldera-config-field">
 		<input type="text" class="block-input field-config magic-tag-enabled caldera-field-bind required" id="{{_id}}_subject" name="{{_name}}[subject]" value="{{subject}}">
@@ -22,7 +22,7 @@
 </div>
 <div class="caldera-config-group">
 	<label>
-		<?php esc_html__('Recipient Name', 'caldera-forms'); ?>
+		<?php esc_html_e('Recipient Name', 'caldera-forms'); ?>
 	</label>
 	<div class="caldera-config-field">
 		<input type="text" class="block-input field-config magic-tag-enabled caldera-field-bind required" id="{{_id}}_recipient_name" name="{{_name}}[recipient_name]" value="{{recipient_name}}">
@@ -30,7 +30,7 @@
 </div>
 <div class="caldera-config-group">
 	<label>
-		<?php esc_html__('Recipient Email', 'caldera-forms'); ?>
+		<?php esc_html_e('Recipient Email', 'caldera-forms'); ?>
 	</label>
 	<div class="caldera-config-field">
 		<input type="text" class="block-input field-config magic-tag-enabled caldera-field-bind required" id="{{_id}}_recipient_email" name="{{_name}}[recipient_email]" value="{{recipient_email}}">
@@ -38,7 +38,7 @@
 </div>
 <div class="caldera-config-group">
 	<label>
-		<?php esc_html__('Message', 'caldera-forms'); ?>
+		<?php esc_html_e('Message', 'caldera-forms'); ?>
 	</label>
 	<div class="caldera-config-field">
 		<textarea rows="6" class="block-input field-config required magic-tag-enabled" name="{{_name}}[message]">{{#if message}}{{message}}{{else}}Hi %recipient_name%.
