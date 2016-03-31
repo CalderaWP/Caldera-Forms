6b78938167007b86a7159f66ea42f01391b67d05
David Cramer <david@digilab.co.za>
2016-03-31 12:44:43 +0200

    version and changelog

--- caldera-core.php
+++ caldera-core.php
@@ -4,7 +4,7 @@
   Plugin URI: https://calderawp.com/caldera-forms/
   Description: Easy to use, grid based responsive form builder for creating simple to complex forms.
   Author: David Cramer
-  Version: 1.3.4
+  Version: 1.3.4.2
   Author URI: https://calderawp.com
   Text Domain: caldera-forms
   GitHub Plugin URI: https://github.com/Desertsnowman/Caldera-Forms/
@@ -20,9 +20,9 @@ if ( ! defined( 'WPINC' ) ) {
 
 define('CFCORE_PATH', plugin_dir_path(__FILE__));
 define('CFCORE_URL', plugin_dir_url(__FILE__));
-define('CFCORE_VER', '1.3.4');
+define('CFCORE_VER', '1.3.4.2');
 define('CFCORE_EXTEND_URL', 'https://api.calderaforms.com/1.0/');
-define( 'CFCORE_BASENAME', plugin_basename( __FILE__ ) );
+define('CFCORE_BASENAME', plugin_basename( __FILE__ ));
 
 /**
  * Caldera Forms DB version
@@ -46,7 +46,7 @@ include_once CFCORE_PATH . 'includes/filter_addon_plugins.php';
 include_once CFCORE_PATH . 'processors/classes/load.php';
 include_once CFCORE_PATH . 'processors/classes/get_data.php';
 
-// init internals of CF urls
+// init internals of CF
 add_action( 'init', array( 'Caldera_Forms', 'init_cf_internal' ) );
 // table builder
 register_activation_hook( __FILE__, array( 'Caldera_Forms', 'activate_caldera_forms' ) );
