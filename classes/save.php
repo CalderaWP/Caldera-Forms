ac175f1f6a01771f882238a53180ffcb027f77c6
Josh Pollock <jpollock412@gmail.com>
2016-04-04 20:40:26 -0400

    Return immediately if caldera_forms_mailer returns an empty or not an array #522

--- classes/save.php
+++ classes/save.php
@@ -332,7 +332,10 @@ class Caldera_Forms_Save_Final {
 		 * @param array $form The form config
 		 */
 		$mail = apply_filters( 'caldera_forms_mailer', $mail, $data, $form);
+		if ( ! is_array( $mail ) ) {
+			return;
 
+		}
 
 		$headers = implode("\r\n", $mail['headers']);
 
