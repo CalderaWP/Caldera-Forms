2619c347717ff894e920ae6e17f9a37f465ccb17
Josh Pollock <jpollock412@gmail.com>
2016-06-03 20:08:43 -0400

    Clean up and document "Caldera_Forms_Entry_Entries" class #617 #524

--- classes/entry/entries.php
+++ classes/entry/entries.php
@@ -1,8 +1,8 @@
 <?php
-use ingot\api\internal\test_group\get;
+
 
 /**
- * Singleton for holding Caldera_Forms_Entry instances currently in use
+ * A collection of entries for one form
  *
  * @package Caldera_Forms
  * @author    Josh Pollock <Josh@CalderaWP.com>
@@ -12,124 +12,447 @@ use ingot\api\internal\test_group\get;
  */
 class Caldera_Forms_Entry_Entries {
 
+	/**
+	 * Form config
+	 *
+	 * @since 1.3.6
+	 *
+	 * @var array
+	 */
 	protected $form;
 
+	/**
+	 * Holds all entries, index by status and then page
+	 *
+	 * @since 1.3.6
+	 *
+	 * @var array
+	 */
 	protected $entries = array();
 
+	/**
+	 * Holds totals by page
+	 *
+	 * @since 1.3.6
+	 *
+	 * @var array
+	 */
 	protected $totals = array();
 
+	/**
+	 * Allowed statuses
+	 *
+	 * @todo need to make sure this matches in other places, use filter or something
+	 *
+	 * @since 1.3.6
+	 *
+	 * @var array
+	 */
 	protected $statuses = array(
 		'active',
 		'pending',
 		'trash'
 	);
 
+	/**
+	 * Total per page
+	 *
+	 * @since 1.3.6
+	 *
+	 * @var int
+	 */
 	protected $perpage;
 
-	public function __construct( array $form,  $perpage  ){
-		$this->form = $form;
+	/**
+	 * Caldera_Forms_Entry_Entries constructor.
+	 *
+	 * @since 1.3.6
+	 *
+	 * @param array $form Form config
+	 * @param int $perpage Number of entries per pags
+	 */
+	public function __construct( array $form, $perpage ) {
+		$this->form    = $form;
 		$this->perpage = $perpage;
 		$this->prepare_entries_prop();
 
 	}
 
-	protected function prepare_entries_prop(){
-		foreach( $this->statuses as $status ){
-			$total = $this->query_total( $status );
-			$this->totals[ $status ] = $total;
-			$pages = ceil( $total / $this->perpage );
-			$this->entries[ $status ] = array();
-			if( 1 == $pages ){
-				$this->entries[ $status ] = array( 1 => array( ) );
-			}else{
-				$this->entries[ $status ] = array_fill( 1, $pages, array() );
-			}
-
-		}
-
-	}
-
-	public function get_page( $page = 1, $status = 'active' ){
+	/**
+	 * Get one page of results
+	 *
+	 * @since 1.3.6
+	 *
+	 * @param int $page
+	 * @param string $status
+	 *
+	 * @return array
+	 */
+	public function get_page( $page = 1, $status = 'active' ) {
 		$page = (int) $page;
-		if( ! in_array( $status, $this->statuses   )
-		   // || ! array_key_exists( $page, $this->entries[ $status ][ $page ])
-		){
+		if ( ! in_array( $status, $this->statuses ) ) {
 			return array();
 		}
 
-		if( empty(  $this->entries[ $status][ $page ] ) ){
+		if ( empty( $this->entries[ $status ][ $page ] ) ) {
 			$this->query_page( $page, $status );
 		}
 
 		return $this->entries[ $status ][ $page ];
 	}
 
-	public function get_rows( $page, $entry_id, $status = 'active' ){
+	/**
+	 * Get an entry row merging entry and field value
+	 *
+	 * This is for use inside of Caldera_Forms_Admin::get_entries() for backwards compat reasons
+	 *
+	 * @since 1.3.6
+	 *
+	 * @param int $page What page of results to get
+	 * @param int $entry_id ID of entry
+	 * @param string $status Optional. Status, default is 'active'
+	 *
+	 * @return array
+	 */
+	public function get_rows( $page, $entry_id, $status = 'active' ) {
 		$page = (int) $page;
-		if( ! in_array( $status, $this->statuses ) ){
+		if ( ! in_array( $status, $this->statuses ) ) {
 			return array();
 		}
 
-		if( ! isset( $this->entries[ $status ][ $page ] ) ){
+		if ( ! isset( $this->entries[ $status ][ $page ] ) ) {
 			$this->query_page( $page, $status );
 		}
 
-		if( ! isset( $this->entries[ $status ][ $page ] ) || ! isset( $this->entries[ $status ][ $page ][ $entry_id ] ) ){
+		if ( ! isset( $this->entries[ $status ][ $page ] ) || ! isset( $this->entries[ $status ][ $page ][ $entry_id ] ) ) {
 			return array();
 		}
 
-		$data =  $_entry = array();
+		$data = $_entry = array();
 		/** @var Caldera_Forms_Entry $entry */
 		$entry = $this->entries[ $status ][ $page ][ $entry_id ];
 
-		foreach( $entry->get_entry()->to_array() as $key => $value ){
-			$_entry[  '_' . $key ] = $value;
+		foreach ( $entry->get_entry()->to_array() as $key => $value ) {
+			$_entry[ '_' . $key ] = $value;
 		}
 
 		/** @var Caldera_Forms_Entry_Field $_field */
-		foreach( $entry->get_fields() as $_field ){
-			$field = array_merge( $_field->to_array(), $_entry  );
+		foreach ( $entry->get_fields() as $_field ) {
+			$field  = array_merge( $_field->to_array(), $_entry );
 			$data[] = (object) $field;
 		}
 
 		return $data;
 
-
-
-
 	}
 
-	public function get_total( $status ){
-		if( ! in_array( $status, $this->statuses   ) ){
+	/**
+	 * Get total number of entries with a given status
+	 *
+	 * @since 1.3.6
+	 *
+	 * @param string $status Which status
+	 *
+	 * @return int
+	 */
+	public function get_total( $status ) {
+		if ( ! in_array( $status, $this->statuses ) ) {
 			return 0;
 		}
 
 		return $this->totals[ $status ];
 	}
 
-	protected function query_page( $page, $status  ){
+	/**
+	 * Query for a page of results
+	 *
+	 * @since 1.3.6
+	 *
+	 * @param int $page What page
+	 * @param string $status Which status
+	 */
+	protected function query_page( $page, $status ) {
 		global $wpdb;
-		$table = $wpdb->prefix ."cf_form_entries";
-		$offset = ($page - 1) * $this->perpage;
-		$limit = $offset . ',' . $this->perpage;
-		$sql = $wpdb->prepare("SELECT * FROM $table WHERE `form_id` = %s AND `status` = %s ORDER BY `datestamp` DESC LIMIT " . $limit . ";", $this->form[ 'ID' ], $status );
+		$table    = $wpdb->prefix . "cf_form_entries";
+		$offset   = ( $page - 1 ) * $this->perpage;
+		$limit    = $offset . ',' . $this->perpage;
+		$sql      = $wpdb->prepare( "SELECT * FROM $table WHERE `form_id` = %s AND `status` = %s ORDER BY `datestamp` DESC LIMIT " . $limit . ";", $this->form[ 'ID' ], $status );
 		$_entries = $wpdb->get_results( $sql );
-		if( ! empty( $_entries ) ){
+		if ( ! empty( $_entries ) ) {
 			$this->entries[ $status ][ $page ] = array();
-			foreach( $_entries as $_entry ){
-				$entry = new Caldera_Forms_Entry_Entry( $_entry );
+			foreach ( $_entries as $_entry ) {
+				$entry                                                  = new Caldera_Forms_Entry_Entry( $_entry );
 				$this->entries[ $status ][ $page ][ (int) $_entry->id ] = new Caldera_Forms_Entry( $this->form, $entry->id, $entry );
 			}
 
 		}
 	}
 
-
-
-	protected function query_total( $status ){
+	/**
+	 * Query for total rows
+	 *
+	 * @since 1.3.6
+	 *
+	 * @param string $status Which status
+	 *
+	 * @return int
+	 */
+	protected function query_total( $status ) {
 		global $wpdb;
-		$sql = $wpdb->prepare("SELECT COUNT(`id`) AS `total` FROM `" . $wpdb->prefix . "cf_form_entries` WHERE `form_id` = %s AND `status` = %s;", $this->form[ 'ID' ], $status );
+		$sql   = $wpdb->prepare( "SELECT COUNT(`id`) AS `total` FROM `" . $wpdb->prefix . "cf_form_entries` WHERE `form_id` = %s AND `status` = %s;", $this->form[ 'ID' ], $status );
 		$total = $wpdb->get_var( $sql );
+
 		return (int) $total;
 	}
+
+	/**
+	 * Populates the entries property of this class with arrays per status and per page as needed.
+	 *
+	 * @since 1.3.6
+	 */
+	protected function prepare_entries_prop() {
+		foreach ( $this->statuses as $status ) {
+			$total                    = $this->query_total( $status );
+			$this->totals[ $status ]  = $total;
+			$pages                    = ceil( $total / $this->perpage );
+			$this->entries[ $status ] = array();
+			if ( 1 == $pages ) {
+				$this->entries[ $status ] = array( 1 => array() );
+			} else {
+				$this->entries[ $status ] = array_fill( 1, $pages, array() );
+			}
+
+		}
+
+	}
+	
+}
+<?php
+
+
+/**
+ * A collection of entries for one form
+ *
+ * @package Caldera_Forms
+ * @author    Josh Pollock <Josh@CalderaWP.com>
+ * @license   GPL-2.0+
+ * @link
+ * @copyright 2016 CalderaWP LLC
+ */
+class Caldera_Forms_Entry_Entries {
+
+	/**
+	 * Form config
+	 *
+	 * @since 1.3.6
+	 *
+	 * @var array
+	 */
+	protected $form;
+
+	/**
+	 * Holds all entries, index by status and then page
+	 *
+	 * @since 1.3.6
+	 *
+	 * @var array
+	 */
+	protected $entries = array();
+
+	/**
+	 * Holds totals by page
+	 *
+	 * @since 1.3.6
+	 *
+	 * @var array
+	 */
+	protected $totals = array();
+
+	/**
+	 * Allowed statuses
+	 *
+	 * @todo need to make sure this matches in other places, use filter or something
+	 *
+	 * @since 1.3.6
+	 *
+	 * @var array
+	 */
+	protected $statuses = array(
+		'active',
+		'pending',
+		'trash'
+	);
+
+	/**
+	 * Total per page
+	 *
+	 * @since 1.3.6
+	 *
+	 * @var int
+	 */
+	protected $perpage;
+
+	/**
+	 * Caldera_Forms_Entry_Entries constructor.
+	 *
+	 * @since 1.3.6
+	 *
+	 * @param array $form Form config
+	 * @param int $perpage Number of entries per pags
+	 */
+	public function __construct( array $form, $perpage ) {
+		$this->form    = $form;
+		$this->perpage = $perpage;
+		$this->prepare_entries_prop();
+
+	}
+
+	/**
+	 * Get one page of results
+	 *
+	 * @since 1.3.6
+	 *
+	 * @param int $page
+	 * @param string $status
+	 *
+	 * @return array
+	 */
+	public function get_page( $page = 1, $status = 'active' ) {
+		$page = (int) $page;
+		if ( ! in_array( $status, $this->statuses ) ) {
+			return array();
+		}
+
+		if ( empty( $this->entries[ $status ][ $page ] ) ) {
+			$this->query_page( $page, $status );
+		}
+
+		return $this->entries[ $status ][ $page ];
+	}
+
+	/**
+	 * Get an entry row merging entry and field value
+	 *
+	 * This is for use inside of Caldera_Forms_Admin::get_entries() for backwards compat reasons
+	 *
+	 * @since 1.3.6
+	 *
+	 * @param int $page What page of results to get
+	 * @param int $entry_id ID of entry
+	 * @param string $status Optional. Status, default is 'active'
+	 *
+	 * @return array
+	 */
+	public function get_rows( $page, $entry_id, $status = 'active' ) {
+		$page = (int) $page;
+		if ( ! in_array( $status, $this->statuses ) ) {
+			return array();
+		}
+
+		if ( ! isset( $this->entries[ $status ][ $page ] ) ) {
+			$this->query_page( $page, $status );
+		}
+
+		if ( ! isset( $this->entries[ $status ][ $page ] ) || ! isset( $this->entries[ $status ][ $page ][ $entry_id ] ) ) {
+			return array();
+		}
+
+		$data = $_entry = array();
+		/** @var Caldera_Forms_Entry $entry */
+		$entry = $this->entries[ $status ][ $page ][ $entry_id ];
+
+		foreach ( $entry->get_entry()->to_array() as $key => $value ) {
+			$_entry[ '_' . $key ] = $value;
+		}
+
+		/** @var Caldera_Forms_Entry_Field $_field */
+		foreach ( $entry->get_fields() as $_field ) {
+			$field  = array_merge( $_field->to_array(), $_entry );
+			$data[] = (object) $field;
+		}
+
+		return $data;
+
+	}
+
+	/**
+	 * Get total number of entries with a given status
+	 *
+	 * @since 1.3.6
+	 *
+	 * @param string $status Which status
+	 *
+	 * @return int
+	 */
+	public function get_total( $status ) {
+		if ( ! in_array( $status, $this->statuses ) ) {
+			return 0;
+		}
+
+		return $this->totals[ $status ];
+	}
+
+	/**
+	 * Query for a page of results
+	 *
+	 * @since 1.3.6
+	 *
+	 * @param int $page What page
+	 * @param string $status Which status
+	 */
+	protected function query_page( $page, $status ) {
+		global $wpdb;
+		$table    = $wpdb->prefix . "cf_form_entries";
+		$offset   = ( $page - 1 ) * $this->perpage;
+		$limit    = $offset . ',' . $this->perpage;
+		$sql      = $wpdb->prepare( "SELECT * FROM $table WHERE `form_id` = %s AND `status` = %s ORDER BY `datestamp` DESC LIMIT " . $limit . ";", $this->form[ 'ID' ], $status );
+		$_entries = $wpdb->get_results( $sql );
+		if ( ! empty( $_entries ) ) {
+			$this->entries[ $status ][ $page ] = array();
+			foreach ( $_entries as $_entry ) {
+				$entry                                                  = new Caldera_Forms_Entry_Entry( $_entry );
+				$this->entries[ $status ][ $page ][ (int) $_entry->id ] = new Caldera_Forms_Entry( $this->form, $entry->id, $entry );
+			}
+
+		}
+	}
+
+	/**
+	 * Query for total rows
+	 *
+	 * @since 1.3.6
+	 *
+	 * @param string $status Which status
+	 *
+	 * @return int
+	 */
+	protected function query_total( $status ) {
+		global $wpdb;
+		$sql   = $wpdb->prepare( "SELECT COUNT(`id`) AS `total` FROM `" . $wpdb->prefix . "cf_form_entries` WHERE `form_id` = %s AND `status` = %s;", $this->form[ 'ID' ], $status );
+		$total = $wpdb->get_var( $sql );
+
+		return (int) $total;
+	}
+
+	/**
+	 * Populates the entries property of this class with arrays per status and per page as needed.
+	 *
+	 * @since 1.3.6
+	 */
+	protected function prepare_entries_prop() {
+		foreach ( $this->statuses as $status ) {
+			$total                    = $this->query_total( $status );
+			$this->totals[ $status ]  = $total;
+			$pages                    = ceil( $total / $this->perpage );
+			$this->entries[ $status ] = array();
+			if ( 1 == $pages ) {
+				$this->entries[ $status ] = array( 1 => array() );
+			} else {
+				$this->entries[ $status ] = array_fill( 1, $pages, array() );
+			}
+
+		}
+
+	}
+	
 }
