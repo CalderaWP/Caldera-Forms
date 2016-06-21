c604a61618274f4819369da08a01ade2f9682cbe
David Cramer <david@digilab.co.za>
2016-06-21 09:20:57 +0200

    wrap toolbar in elements to stop overlapping #627

--- ui/entries_toolbar.php
+++ ui/entries_toolbar.php
@@ -3,50 +3,54 @@
 		?>
 		<div class="caldera-entry-exporter" style="display:none;">
 			<?php wp_nonce_field( 'cf_toolbar', 'cf_toolbar_actions' ); ?>
-			<button type="button" class="button hide-forms" title="<?php esc_html_e('Toggle Forms List', 'caldera-forms'); ?>" style="padding: 3px; margin-top: 1px; margin-right: 18px; color: rgb(143, 143, 143);"><span class="dashicons dashicons-admin-collapse"></span></button>
-			<span class="toggle_option_preview" style="">			
+			<span class="cf-tools-row">
+				<button id="cf_forms_toggle" type="button" class="button hide-forms" title="<?php esc_html_e('Toggle Forms List', 'caldera-forms'); ?>" style="padding: 3px; margin-top: 1px; margin-right: 18px; color: rgb(143, 143, 143);"><span class="dashicons dashicons-admin-collapse"></span></button>
+				
+				<span class="toggle_option_preview">
 
-				<button type="button" class="status_toggles button button-primary ajax-trigger" style="margin-top: 1px;"
-					data-before="cf_set_limits"
-					data-action="browse_entries"
-					data-target="#form-entries-viewer"
-					data-form=""
-					data-template="#forms-list-alt-tmpl"
-					data-load-class="spinner"
-					data-active-class="button-primary"
-					data-group="status_nav"
-					data-callback="setup_pagination"
-					data-page="1"
-				    data-nonce="<?php echo wp_create_nonce( 'view_entries' ); ?>"
-					data-status="active"
-				><?php esc_html_e('Active', 'caldera-forms'); ?> <span class="current-status-count"></span></button>
-				<button type="button" class="status_toggles button ajax-trigger" style="margin-top: 1px; margin-right: 10px;"
-					data-before="cf_set_limits"
-					data-action="browse_entries"
-					data-target="#form-entries-viewer"
-					data-form=""
-					data-template="#forms-list-alt-tmpl"
-					data-load-class="spinner"
-					data-active-class="button-primary"
-					data-group="status_nav"
-					data-callback="setup_pagination"
-					data-page="1"
-				    data-nonce="<?php echo wp_create_nonce( 'view_entries' ); ?>"
-			        data-status="trash"
-				><?php esc_html_e('Trash'); ?> <span class="current-status-count"></span></button>
+					<button type="button" class="status_toggles button button-primary ajax-trigger" style="margin-top: 1px;"
+						data-before="cf_set_limits"
+						data-action="browse_entries"
+						data-target="#form-entries-viewer"
+						data-form=""
+						data-template="#forms-list-alt-tmpl"
+						data-load-class="spinner"
+						data-active-class="button-primary"
+						data-group="status_nav"
+						data-callback="setup_pagination"
+						data-page="1"
+					    data-nonce="<?php echo wp_create_nonce( 'view_entries' ); ?>"
+						data-status="active"
+					><?php esc_html_e('Active', 'caldera-forms'); ?> <span class="current-status-count"></span></button>
+					<button type="button" class="status_toggles button ajax-trigger" style="margin-top: 1px; margin-right: 10px;"
+						data-before="cf_set_limits"
+						data-action="browse_entries"
+						data-target="#form-entries-viewer"
+						data-form=""
+						data-template="#forms-list-alt-tmpl"
+						data-load-class="spinner"
+						data-active-class="button-primary"
+						data-group="status_nav"
+						data-callback="setup_pagination"
+						data-page="1"
+					    data-nonce="<?php echo wp_create_nonce( 'view_entries' ); ?>"
+				        data-status="trash"
+					><?php esc_html_e('Trash'); ?> <span class="current-status-count"></span></button>
+				</span>
+
+				<?php esc_html_e('Show', 'caldera-forms'); ?> <input id="cf-entries-list-items" type="number" value="<?php echo $entry_perpage; ?>" class="screen-per-page"> &nbsp;
 			</span>
+			<span class="cf-tools-row">
+				<a href="" class="button caldera-forms-entry-exporter">
+					<?php esc_html_e('Export Entries', 'caldera-forms'); ?>
+				</a>
 
-			<span><?php esc_html_e('Show', 'caldera-forms'); ?> <input id="cf-entries-list-items" type="number" value="<?php echo $entry_perpage; ?>" class="screen-per-page"> &nbsp;
-
-			<a href="" class="button caldera-forms-entry-exporter">
-				<?php esc_html_e('Export Entries', 'caldera-forms'); ?>
-			</a>
-
-			<select id="cf_bulk_action" name="action" style="vertical-align: initial;">
-			</select>
-			<button type="button" class="button cf-bulk-action">
-				<?php esc_html_e('Apply', 'caldera-forms'); ?>
-			</button>
+				<select id="cf_bulk_action" name="action" style="vertical-align: initial;">
+				</select>
+				<button type="button" class="button cf-bulk-action">
+					<?php esc_html_e('Apply', 'caldera-forms'); ?>
+				</button>
+			</span>
 
 		</div>
 
