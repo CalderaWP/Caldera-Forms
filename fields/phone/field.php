4c498c38fb4a44df4e96fdcafb243746b5db821e
Josh Pollock <Josh@JoshPress.net>
2016-08-31 20:25:46 -0400

    use tel input type for phone fields #778

--- fields/phone/field.php
+++ fields/phone/field.php
@@ -13,7 +13,7 @@ if( $field['config']['type'] == 'international' ){
 ?><?php echo $wrapper_before; ?>
 	<?php echo $field_label; ?>
 	<?php echo $field_before; ?>
-		<input <?php echo $field_placeholder; ?> type="text" data-inputmask="'mask': '<?php echo esc_attr( $mask ); ?>'" data-field="<?php echo esc_attr( $field_base_id ); ?>" class="<?php echo esc_attr( $field_class ); ?>" id="<?php echo esc_attr( $field_id ); ?>" name="<?php echo esc_attr( $field_name ); ?>" value="<?php echo esc_attr( $field_value ); ?>" <?php echo $field_required; ?> <?php echo $field_structure['aria']; ?>>
+		<input <?php echo $field_placeholder; ?> type="tel" data-inputmask="'mask': '<?php echo esc_attr( $mask ); ?>'" data-field="<?php echo esc_attr( $field_base_id ); ?>" class="<?php echo esc_attr( $field_class ); ?>" id="<?php echo esc_attr( $field_id ); ?>" name="<?php echo esc_attr( $field_name ); ?>" value="<?php echo esc_attr( $field_value ); ?>" <?php echo $field_required; ?> <?php echo $field_structure['aria']; ?>>
 		<?php echo $field_caption; ?>
 	<?php echo $field_after; ?>
 <?php echo $wrapper_after; ?>
