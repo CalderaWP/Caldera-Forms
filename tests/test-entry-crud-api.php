aaaf9fcbca584b3a5141d0a16b26daf94f873194
Josh Pollock <Josh@JoshPress.net>
2016-10-01 19:59:02 -0400

    #854 set up unit test case for rest api endpoints

---
+++ tests/test-entry-crud-api.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Test the Entry CRUD REST API
+ *
+ * @package   Caldera_Forms
+ * @author    Josh Pollock <Josh@CalderaWP.com>
+ * @license   GPL-2.0+
+ * @link
+ * @copyright 2016 CalderaWP LLC
+ */
+class Test_Caldera_Forms_Entry_Crud_API extends CF_Rest_Test_Case {
+
+
+	protected $route_name = 'entries';
+
+}
