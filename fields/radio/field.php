eeea2e8fc4feceb5275811c63de14921086adece
Josh Pollock <Josh@JoshPress.net>
2017-02-27 19:54:53 -0500

    use strict equality check for checkbox/toggle switch when setting checked #1294

--- fields/radio/field.php
+++ fields/radio/field.php
@@ -26,6 +26,11 @@
 
 		<?php }else{
 			foreach($field['config']['option'] as $option_key=>$option){
+				$checked = false;
+				if( $field_value === $option['value'] || $field_value === $option_key ) {
+					$checked = true;
+				}
+
 				if(!isset($option['value'])){
 					$option['value'] = $option['label'];
 				}
@@ -34,7 +39,7 @@
 				<?php if(empty($field['config']['inline'])){ ?>
 				<div class="radio">
 				<?php } ?>
-				<label<?php if(!empty($field['config']['inline'])){ ?> class="radio-inline"<?php } ?> data-label="<?php echo esc_attr( $option['label'] ); ?>" for="<?php echo esc_attr( $field_id . '_' . $option_key ); ?>"><input type="radio" id="<?php echo esc_attr( $field_id . '_' . $option_key ); ?>" data-field="<?php echo esc_attr( $field_base_id ); ?>" class="<?php echo esc_attr( $field_id . $req_class ); ?>" name="<?php echo esc_attr( $field_name ); ?>" value="<?php echo esc_attr( $option['value'] ); ?>" <?php if( $field_value == $option['value'] || $field_value == $option_key ){ ?>checked="checked"<?php } ?> <?php echo $field_required; ?>> <?php echo $option['label']; ?></label>
+				<label<?php if(!empty($field['config']['inline'])){ ?> class="radio-inline"<?php } ?> data-label="<?php echo esc_attr( $option['label'] ); ?>" for="<?php echo esc_attr( $field_id . '_' . $option_key ); ?>"><input type="radio" id="<?php echo esc_attr( $field_id . '_' . $option_key ); ?>" data-field="<?php echo esc_attr( $field_base_id ); ?>" class="<?php echo esc_attr( $field_id . $req_class ); ?>" name="<?php echo esc_attr( $field_name ); ?>" value="<?php echo esc_attr( $option['value'] ); ?>" <?php if( $checked ){ ?>checked="checked"<?php } ?> <?php echo $field_required; ?>> <?php echo $option['label']; ?></label>
 				<?php if(empty($field['config']['inline'])){ ?>
 				</div>
 				<?php } ?>
