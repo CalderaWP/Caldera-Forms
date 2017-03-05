3fff308b2c2840ad01cf1bb9d8bd96d41aac32ec
Josh Pollock <Josh@JoshPress.net>
2017-03-05 18:37:16 -0500

    impliemnt UI generator for dropdown  fields #1358

--- fields/dropdown/config_template.php
+++ fields/dropdown/config_template.php
@@ -1,8 +1,3 @@
-<div class="caldera-config-group">
-	<label for="{{_id}}_placeholder">
-        <?php esc_html_e('Placeholder', 'caldera-forms'); ?>
-    </label>
-	<div class="caldera-config-field">
-		<input type="text" id="{{_id}}_placeholder" class="block-input field-config" name="{{_name}}[placeholder]" value="{{placeholder}}">
-	</div>
-</div>
+<?php
+
+echo Caldera_Forms_Admin_UI::fields( array( 'placeholder' ), 'dropdown' );
