c1bf42f4b1ca01d8818dfdcf3567e9b1fba4dc5e
Josh Pollock <Josh@JoshPress.net>
2017-03-04 20:47:10 -0500

    Add a store for forms in editor #1139

--- assets/js/api/stores.js
+++ assets/js/api/stores.js
@@ -93,3 +93,207 @@ function CFEntriesStoreFactory( formId, entries ){
         }
     }
 }
+
+/**
+ * A factory for creating a form store for use in form  editor
+ *
+ * @since 1.5.1
+ *
+ * @param form
+ * @returns {{getFields: getFields, getField: getField, getFieldType: getFieldType, addField: addField, updateField: updateField, getConditionals: getConditionals, getConditional: getConditional, getProcessors: getProcessors, getProcessor: getProcessor}}
+ * @constructor
+ */
+function CFFormEditStore( form ) {
+    /**
+     * Check if object has a key
+     *
+     * @since 1.5.1
+     *
+     * @param object
+     * @param key
+     * @returns {boolean}
+     */
+    function has(object, key) {
+        return object ? hasOwnProperty.call(object, key) : false;
+    }
+
+    /**
+     * Check if is empty object
+     *
+     * @since 1.5.1
+     *
+     * @param obj
+     * @returns {boolean}
+     */
+    function emptyObject(obj) {
+        return Object.keys(obj).length === 0 && obj.constructor === Object;
+    }
+
+    /**
+     * Keys of field config
+     *
+     * @since 1.5.1
+     *
+     * @type {string[]}
+     */
+    var fieldKeys = [
+        'ID',
+        'type',
+        'label',
+        'slug',
+        'config',
+        'caption',
+        'custom_class',
+        'default',
+        'conditions'
+    ];
+
+    /**
+     * Create a new field config object
+     *
+     * @since 1.5.1
+     *
+     * @param fieldId
+     * @param type
+     * @returns {{ID: *, type: *, config: {}}}
+     */
+    function fieldFactory(fieldId, type) {
+        var field = {
+            ID: fieldId,
+            type: type,
+            config: {}
+        };
+
+       fieldKeys.forEach( function (index) {
+            if( ! has(field, index ) ){
+                field[index] = '';
+            }
+        });
+
+        return field;
+    }
+
+
+    return {
+        /**
+         * Get all fields of form
+         *
+         * @returns {*}
+         */
+        getFields : function(){
+            return form.fields;
+        },
+        /**
+         * Get a field of a form
+         *
+         *  @since 1.5.1
+         *
+         * @param id
+         * @returns {*}
+         */
+        getField : function ( id ) {
+            if( has( form.fields, id ) ){
+                return form.fields[id];
+            }
+            return {}
+        },
+        /**
+         * Get field type by field ID
+         *
+         *  @since 1.5.1
+         *
+         * @param id
+         * @returns {*}
+         */
+        getFieldType: function ( id ) {
+            var field = this.getField(id);
+            if( field ){
+                return field.type;
+            }
+            return false;
+        },
+        /**
+         * Add a field to collection
+         *
+         *  @since 1.5.1
+         *
+         * @param fieldId
+         * @param fieldType
+         * @returns {*|{}}
+         */
+        addField : function (fieldId,fieldType) {
+            form.fields[fieldId] = fieldFactory(fieldId,fieldType);
+            return this.getField(fieldId);
+        },
+        /**
+         * Update field in collection
+         *
+         *  @since 1.5.1
+         *
+         * @param id
+         * @param key
+         * @param data
+         * @returns {*}
+         */
+        updateField: function (id, key, data ) {
+            var field = this.getField(id);
+            if( ! emptyObject(field) ){
+                if( fieldKeys.indexOf( key ) ){
+                    form.fields[id][key] = data;
+                    return this.getField(id);
+                }
+
+            }
+            return false;
+        },
+        /**
+         * Get conditional groups of form
+         *
+         * @since 1.5.1
+         *
+         * @returns {*}
+         */
+        getConditionals : function () {
+            return form.conditional_groups.conditions;
+        },
+        /**
+         * Get a conditional group by ID
+         *
+         *  @since 1.5.1
+         *
+         * @param id
+         * @returns {*}
+         */
+        getConditional : function ( id ) {
+            if( has( form.conditional_groups.conditions, id ) ){
+                return form.conditional_groups.conditions[id];
+            }
+            return {}
+        },
+        /**
+         * Get processors of form
+         *
+         * @since 1.5.1
+         *
+         * @returns {*}
+         */
+        getProcessors : function() {
+            return form.processors;
+        },
+        /**
+         * Get a form processor
+         *
+         * @since 1.5.1
+         *
+         * @param id
+         * @returns {*}
+         */
+        getProcessor: function ( id ) {
+            if( has( form.processors, id )){
+                return form.processors[id];
+            }
+            return {}
+        }
+
+    }
+}
