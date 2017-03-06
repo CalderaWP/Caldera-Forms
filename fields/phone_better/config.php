e357260f90e65dacb0b32935fcbb8ac3a8084b64
Josh Pollock <Josh@JoshPress.net>
2017-03-05 19:12:37 -0500

    impliemnt UI generator for phone btter fields #1358

--- fields/phone_better/config.php
+++ fields/phone_better/config.php
@@ -1,31 +1,14 @@
-<div class="caldera-config-group">
-	<label for="{{_id}}_placeholder">
-        <?php esc_html_e('Placeholder', 'caldera-forms'); ?>
-    </label>
-	<div class="caldera-config-field">
-		<input type="text" id="{{_id}}_placeholder" class="block-input field-config" name="{{_name}}[placeholder]" value="{{placeholder}}">
-	</div>
-</div>
-
-<div class="caldera-config-group">
-	<label for="{{_id}}_default">
-        <?php esc_html_e('Default', 'caldera-forms'); ?>
-    </label>
-	<div class="caldera-config-field">
-		<input type="text" id="{{_id}}_default" class="block-input field-config" name="{{_name}}[default]" value="{{default}}">
-	</div>
-</div>
-
-<div class="caldera-config-group">
-	<fieldset>
-		<legend>
-			<?php esc_html_e('Use Country Code', 'caldera-forms'); ?>
-		</legend>
-
-		<input type="checkbox" id="{{_id}}_nationalMode" class="field-config" name="{{_name}}[nationalMode]" {{#if nationalMode}}checked="checked"{{/if}}">
-		<label for="{{_id}}_nationalMode">
-			<?php esc_html_e( 'If not checked, formatting will be for local use, without country code', 'caldera-forms' ); ?>
-		</label>
-	</fieldset>
-</div>
+<?php
+$fields = array(
+	'placeholder',
+	'default',
+	Caldera_Forms_Admin_UI::checkbox_field(
+		'nationalMode',
+		__( 'Use Country Code', 'caldera-forms' ),
+		array(
+			'nationalMode' => __( 'Enable', 'caldera-forms' )
+		)
+	)
+);
+Caldera_Forms_Admin_UI::fields( $fields, 'phone_better' );
 
