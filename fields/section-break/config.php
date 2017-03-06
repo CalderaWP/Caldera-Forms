445875dc706d74323425b0fb259845c589b94264
Josh Pollock <Josh@JoshPress.net>
2017-03-05 19:32:45 -0500

    impliemnt UI generator for section break fields #1358

--- fields/section-break/config.php
+++ fields/section-break/config.php
@@ -2,16 +2,17 @@
 if( ! defined( 'ABSPATH' ) ){
     exit;
 }
-?>
+$fields = array(
+	Caldera_Forms_Admin_UI::number_field(
+		'width',
+		__( 'Width', 'caldera-forms' ),
+		__( 'Width of section break element, as a percentage.', 'caldera-forms' ),
+		array(
+			'min' => 1,
+			'max' => 100
+		)
 
-<div class="caldera-config-group">
-    <label for="{{_name}}[width]">
-        <?php esc_html_e('Width', 'caldera-forms' ); ?>
-    </label>
-    <div class="caldera-config-field">
-        <input type="number" id="{{_name}}[width]" min="1" max="100" class="block-input field-config magic-tag-enabled" name="{{_name}}[width]" value="{{width}}" aria-describedby="{{_name}}[width]-description"/>
-    </div>
-    <p class="description" id="{{_name}}[width]-description">
-        <?php esc_html_e('Width of section break element, as a percentage.', 'caldera-forms' ); ?>
-    </p>
-</div>
+	),
+);
+echo Caldera_Forms_Admin_UI::fields( $fields, 'section-break' );
+
