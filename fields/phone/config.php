d53fd4dcffc4ecc45b7b1795ab3b92e5c119bb89
Josh Pollock <Josh@JoshPress.net>
2017-03-05 19:11:12 -0500

    impliemnt UI generator for phone fields #1358

--- fields/phone/config.php
+++ fields/phone/config.php
@@ -1,19 +1,12 @@
-<div class="caldera-config-group">
-	<label for="{{_id}}_placeholder">
-        <?php esc_html_e('Placeholder', 'caldera-forms'); ?>
-    </label>
-	<div class="caldera-config-field">
-		<input type="text" id="{{_id}}_placeholder" class="block-input field-config" name="{{_name}}[placeholder]" value="{{placeholder}}">
-	</div>
-</div>
-<div class="caldera-config-group">
-	<label for="{{_id}}_default">
-        <?php esc_html_e('Default', 'caldera-forms'); ?>
-    </label>
-	<div class="caldera-config-field">
-		<input type="text" id="{{_id}}_default" class="block-input field-config" name="{{_name}}[default]" value="{{default}}">
-	</div>
-</div>
+<?php
+$fields = array(
+	'placeholder',
+	'default'
+);
+Caldera_Forms_Admin_UI::fields( $fields, 'phone' );
+
+?>
+
 <div class="caldera-config-group">
 	<fieldset>
 		<legend>
