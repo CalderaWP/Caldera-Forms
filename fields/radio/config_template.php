e330bb6dfff6a2b09bdf7e182b4d107e8b3ee783
Josh Pollock <Josh@JoshPress.net>
2017-03-05 19:14:05 -0500

    impliemnt UI generator for radio fields #1358

--- fields/radio/config_template.php
+++ fields/radio/config_template.php
@@ -1,9 +1,12 @@
-<div class="caldera-config-group">
+<?php
 
-	<div class="caldera-config-field">
-		<label for="{{_id}}_inline">
-            <input id="{{_id}}_inline" type="checkbox" class="field-config" name="{{_name}}[inline]" value="1" {{#if inline}}checked="checked"{{/if}}>
-            <?php esc_html_e('Inline', 'caldera-forms'); ?>
-        </label>
-	</div>
-</div>
+$fields = array(
+	Caldera_Forms_Admin_UI::checkbox_field(
+		'inline',
+		__( 'Inline Options', 'caldera-forms' ),
+		array(
+			'inline' => __( 'Inline Options', 'caldera-forms' ),
+		)
+	)
+);
+echo Caldera_Forms_Admin_UI::fields( $fields, 'radio' );
