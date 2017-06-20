7c72132ae80023692852bf2893d533c0f420ed37
Josh Pollock <Josh@JoshPress.net>
2017-06-20 14:24:41 -0400

    inital work on utm fields #1677

---
+++ classes/field/utm.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * Class Caldera_Forms_Field_Utm
+ */
+class Caldera_Forms_Field_Utm {
+
+	public static function handle_attrs(){
+
+	}
+
+	public static function config( $field ){
+
+	}
+
+}
