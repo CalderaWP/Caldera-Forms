e2f95fead2b35a06f317cec996788fa4bd16f97d
Nicolas Figueira <nico.figueira.mahe@gmail.com>
2017-07-27 12:38:37 +0200

    Issue #1783 Add a button to return to last Form state

--- ui/edit.php
+++ ui/edit.php
@@ -698,6 +698,17 @@ function field_line_template($id = '{{id}}', $label = '{{label}}', $group = '{{g
 		</a>
 	<?php
 	}else{ ?>
+
+		<a
+			href="<?php echo esc_url( Caldera_Forms_Admin::form_edit_link($element[ 'ID' ] )); ?>"
+		    class="button caldera-header-return-button"
+			id="caldera-forms-return-from-revision"
+		>
+			<?php esc_html_e( 'Return to last Form state', 'caldera-forms' ); ?>
+			<span id="save_indicator" class="spinner" style="position: absolute; right: -33px;"></span>
+
+		</a>
+
 		<a
 			href="#"
 		    class="button button-primary caldera-header-restore-button"
@@ -1191,54 +1202,4 @@ var preset_options = <?php echo json_encode( $option_presets ); ?>
 <script type="text/javascript">
 	jQuery('.error,.notice,.notice-error').remove();
 </script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
