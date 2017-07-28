05533ea82edb7fb325069e55ed59bbd2d8ec4e59
pedro-mendonca <ped.gaspar@gmail.com>
2017-07-28 12:06:18 +0100

    Add missing textdomain

--- fields/text/config.php
+++ fields/text/config.php
@@ -7,7 +7,7 @@
 </div>
 <div class="caldera-config-group">
 	<label for="{{_id}}_default">
-		<?php esc_html_e('Default'); ?>
+		<?php esc_html_e('Default', 'caldera-forms'); ?>
 	</label>
 	<div class="caldera-config-field">
 		<input type="text" id="{{_id}}_default" class="block-input field-config magic-tag-enabled" name="{{_name}}[default]" value="{{default}}">
