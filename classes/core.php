e77e4c8b56b4457c95c33fd49e488edcaa0a4959
josh <josh@calderawp.com>
2017-10-31 22:14:31 -0400

    use cached data if possible when using Caldera_Forms::get_field_data() by entry_id #1990

--- classes/core.php
+++ classes/core.php
@@ -2228,27 +2228,42 @@ class Caldera_Forms {
 		// entry fetch
 		if ( ! empty( $entry_id ) && isset( $form[ 'fields' ][ $field_id ] ) ) {
 
-			global $wpdb;
+			if( ! empty( $processed_data[ $indexkey ][ $field_id ] ) ){
+				$entry = $processed_data[ $indexkey ][ $field_id ];
+			}elseif ( ! empty( $processed_data[ $form[ 'ID' ] ] ) && ! empty( $processed_data[ $form[ 'ID' ] ][ '_entry_id' ] ) ){
+				if( $processed_data[ $form[ 'ID' ] ][ '_entry_id' ] === $entry_id ){
+					$processed_data[ $indexkey ] = $processed_data[ $form[ 'ID' ] ];
+					if( ! empty( $processed_data[ $indexkey ][ $field_id ]  ) ){
+						$entry = $processed_data[ $indexkey ][ $field_id ];
+					}
+				}
+			}
 
-			$entry = $wpdb->get_results( $wpdb->prepare( "
+			if( ! $entry ){
+				global $wpdb;
+
+				$entry = $wpdb->get_results( $wpdb->prepare( "
 				SELECT `value` FROM `" . $wpdb->prefix . "cf_form_entry_values` WHERE `entry_id` = %d AND `field_id` = %s AND `slug` = %s", $entry_id, $field_id, $form[ 'fields' ][ $field_id ][ 'slug' ] ), ARRAY_A );
+
+				if ( ! empty( $entry ) ) {
+					if ( count( $entry ) > 1 ) {
+						$out = array();
+						foreach ( $entry as $item ) {
+							$out[] = $item[ 'value' ];
+						}
+						$processed_data[ $indexkey ][ $field_id ] = $out;
+					} else {
+						$processed_data[ $indexkey ][ $field_id ] = $entry[ 0 ][ 'value' ];
+					}
+				} else {
+					$processed_data[ $indexkey ][ $field_id ] = null;
+				}
+			}
 
 			//allow plugins to alter the value
 			$entry = apply_filters( 'caldera_forms_get_field_entry', $entry, $field_id, $form, $entry_id );
 
-			if ( ! empty( $entry ) ) {
-				if ( count( $entry ) > 1 ) {
-					$out = array();
-					foreach ( $entry as $item ) {
-						$out[] = $item[ 'value' ];
-					}
-					$processed_data[ $indexkey ][ $field_id ] = $out;
-				} else {
-					$processed_data[ $indexkey ][ $field_id ] = $entry[ 0 ][ 'value' ];
-				}
-			} else {
-				$processed_data[ $indexkey ][ $field_id ] = null;
-			}
+
 
 			return $processed_data[ $indexkey ][ $field_id ];
 
