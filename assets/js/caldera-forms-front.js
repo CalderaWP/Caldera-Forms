3c14e9275fd8be7a00dfcaf018243a0b8e033b9e
josh <josh@calderawp.com>
2017-11-03 18:10:39 -0400

    fix syntax error #2051

--- assets/js/caldera-forms-front.js
+++ assets/js/caldera-forms-front.js
@@ -1,4 +1,4 @@
-/*! GENERATED SOURCE FILE caldera-forms - v1.5.7-b-1 - 2017-11-02 *//**
+/*! GENERATED SOURCE FILE caldera-forms - v1.5.7-b-1 - 2017-11-03 *//**
  * Simple event bindings for form state
  *
  * In general, access through CFState.events() not directly.
