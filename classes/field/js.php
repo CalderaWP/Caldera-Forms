9d6aa3ae2a85cb8c6d8fa4f7e2232af0846e9b69
josh <josh@calderawp.com>
2017-12-13 17:11:55 -0500

    set calc default to field default if possible when calc default isn't set #2082

--- classes/field/js.php
+++ classes/field/js.php
@@ -759,9 +759,11 @@ class Caldera_Forms_Field_JS implements JsonSerializable {
 			$calc_default = Caldera_Forms_Field_Util::get_default_calc_value( $field, $this->form );
 			if( is_bool( $calc_default ) ){
 				$calc_default = (bool) $calc_default;
-			}elseif( ! is_numeric( $calc_default ) ){
+			}elseif( ! is_numeric( $calc_default ) && ! is_numeric( $default ) ){
 				$calc_default = 0;
-			}
+			}else{
+			    $calc_default = $default;
+            }
 
 			$this->fields[ 'calcDefaults' ][ $id_attr ] = (float) $calc_default;
 		}else{
