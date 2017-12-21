d370810d9384fcb8cc7d3dc091415a3849aa1857
Josh Pollock <josh@calderawp.com>
2017-12-21 15:39:28 -0500

    fix tested up to

--- assets/js/entry-viewer-2.js
+++ assets/js/entry-viewer-2.js
@@ -1,4 +1,4 @@
-/*! GENERATED SOURCE FILE caldera-forms - v1.5.8-b-4 - 2017-12-21 *//**
+/*! GENERATED SOURCE FILE caldera-forms - v1.5.8 - 2017-12-21 *//**
  * API Client for Caldera Forms API for a single form
  *
  * @since 1.5.0
