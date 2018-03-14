42ce7fac9b733c46a06a8d6707ba432a6c83a2db
Josh Pollock <josh@calderawp.com>
2018-03-14 14:51:49 -0400

    setup rules

--- classes/csv/util.php
+++ classes/csv/util.php
@@ -3,78 +3,84 @@
 /**
  * Utility functions for CSV related work
  *
- * @package Caldera_Forms
+ * @package   Caldera_Forms
  * @author    Josh Pollock <Josh@CalderaWP.com>
  * @license   GPL-2.0+
  * @link
  * @copyright 2016 CalderaWP LLC
  */
-class Caldera_Forms_CSV_Util {
+class Caldera_Forms_CSV_Util
+{
 
-	/**
-	 * Get the character encoding for CSV files
-	 *
-	 * @since 1.4.4
-	 *
-	 * @param array $form Form config. Technically optional, but should always be passed for benefit of filter.
-	 *
-	 * @return string
-	 */
-	public static function character_encoding( array  $form = array() ){
+    /**
+     * Get the character encoding for CSV files
+     *
+     * @since 1.4.4
+     *
+     * @param array $form Form config. Technically optional, but should always be passed for benefit of filter.
+     *
+     * @return string
+     */
+    public static function character_encoding(array $form = [])
+    {
+        /*
+            * Change the character encoding for CSV
+            *
+            * Used for entry export and email attachments
+            *
+            * @since 1.4.4
+            *
+            * @param string $encoding. Default is 'utf-8'
+            * @param array $form Current form configuration. Might be empty
+         */
+        return apply_filters('caldera_forms_csv_character_encoding', 'utf-8', $form);
 
-		/**
-		 * Change the character encoding for CSV
-		 *
-		 * Used for entry export and email attachments
-		 *
-		 * @since 1.4.4
-		 *
-		 * @param string $encoding. Default is 'utf-8'
-		 * @param array $form Current form configuration. Might be empty
-		 */
-		return apply_filters( 'caldera_forms_csv_character_encoding', 'utf-8', $form );
-	}
+    }//end character_encoding()
 
-	/**
-	 * Check if CSV export should show time in localized or UTC time.
-	 *
-	 * @since 1.5.6
-	 *
-	 * @param array $form
-	 *
-	 * @return bool
-	 */
-	public static function should_localize_time( array  $form = array() ){
-		/**
-		 * Change if localized or UTC timestamp should be used in CSV export
-		 *
-		 * @since 1.5.6
-		 *
-		 * @param bool $localize. Default is false. Change to true to use localized time, instead of UTC.
-		 * @param array $form Current form configuration. Might be empty
-		 */
-		return apply_filters( 'caldera_forms_csv_localize_time', false, $form );
-	}
+    /**
+     * Check if CSV export should show time in localized or UTC time.
+     *
+     * @since 1.5.6
+     *
+     * @param array $form
+     *
+     * @return boolean
+     */
+    public static function should_localize_time(array $form = [])
+    {
+        /*
+            * Change if localized or UTC timestamp should be used in CSV export
+            *
+            * @since 1.5.6
+            *
+            * @param bool $localize. Default is false. Change to true to use localized time, instead of UTC.
+            * @param array $form Current form configuration. Might be empty
+         */
+        return apply_filters('caldera_forms_csv_localize_time', false, $form);
 
-	/**
-	 * Get the file type for CSV -- tsv or csv
-	 *
-	 * @since 1.5.6
-	 *
-	 * @param array $form Form config. Technically optional, but should always be passed for benefit of filter.
-	 *
-	 * @return string
-	 */
-	public static function file_type( array  $form = array() ){
+    }//end should_localize_time()
 
-		/**
-		 * Change the file type for CSV -- tsv or csv
-		 *
-		 * @since 1.5.6
-		 *
-		 * @param string $type. Default is 'csv'
-		 * @param array $form Current form configuration. Might be empty
-		 */
-		return apply_filters( 'caldera_forms_csv_file_type', 'csv', $form );
-	}
-}
+    /**
+     * Get the file type for CSV -- tsv or csv
+     *
+     * @since 1.5.6
+     *
+     * @param array $form Form config. Technically optional, but should always be passed for benefit of filter.
+     *
+     * @return string
+     */
+    public static function file_type(array $form = [])
+    {
+        /*
+            * Change the file type for CSV -- tsv or csv
+            *
+            * @since 1.5.6
+            *
+            * @param string $type. Default is 'csv'
+            * @param array $form Current form configuration. Might be empty
+         */
+        return apply_filters('caldera_forms_csv_file_type', 'csv', $form);
+
+    }//end file_type()
+
+}//end class
