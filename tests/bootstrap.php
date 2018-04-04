a96b1fd755356fc4647374235967986d3ad4cd57
Josh Pollock <josh@calderawp.com>
2018-04-04 16:05:57 -0400

    include traits before the test cases #2404

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -32,17 +32,19 @@ include_once( dirname( __FILE__ ) . '/includes/forms/simple-form-with-just-a-tex
 
 create_testing_db_tables();
 
+//include traits
+include_once( dirname( __FILE__ ) . '/includes/traits/has-mock-form.php' );
+include_once( dirname( __FILE__ ) . '/includes/traits/imports-form.php' );
+include_once( dirname( __FILE__ ) . '/includes/traits/has-data.php' );
+include_once( dirname( __FILE__ ) . '/includes/traits/submits-contact-form.php' );
+
 //include test case
 include_once( dirname( __FILE__ ) . '/includes/cf-test-case.php' );
 include_once( dirname( __FILE__ ) . '/includes/cf-test-case.php' );
 include_once( dirname( __FILE__ ) . '/includes/cf-mailer-test-case.php' );
 include_once( dirname( __FILE__ ) . '/includes/cf-rest-test-case.php' );
 
-//include traits
-include_once( dirname( __FILE__ ) . '/includes/traits/has-mock-form.php' );
-include_once( dirname( __FILE__ ) . '/includes/traits/imports-form.php' );
-include_once( dirname( __FILE__ ) . '/includes/traits/has-data.php' );
-include_once( dirname( __FILE__ ) . '/includes/traits/submits-contact-form.php' );
+
 
 
 /**
