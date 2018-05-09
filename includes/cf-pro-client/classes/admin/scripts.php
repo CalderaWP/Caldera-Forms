65ce3a5a1e92dd9ccdf74891b765fda168754066
Josh Pollock <josh@calderawp.com>
2018-05-09 14:46:36 -0400

    fix parts of #2167 that got lost in merge

--- includes/cf-pro-client/classes/admin/scripts.php
+++ includes/cf-pro-client/classes/admin/scripts.php
@@ -38,31 +38,27 @@ class scripts {
 		return $this->assets_url;
 	}
 
+	/**
+	 * @param string $view_dir @deprecated
+	 * @param null $context Default is for admin page. Pass "tab" for use in CF admin.
+	 * @param bool $enqueue_admin
+	 * @return string
+	 */
 	public function webpack( $view_dir, $context = null, $enqueue_admin = true ){
-		$inline = \Caldera_Forms_Render_Util::create_cdata('var CF_PRO_ADMIN= ' . wp_json_encode( $this->data() ) . ';' );
+		\Caldera_Forms_Render_Assets::maybe_register();
 		if ( $enqueue_admin ) {
 			wp_enqueue_style( \Caldera_Forms_Admin_Assets::slug( 'admin', false ), \Caldera_Forms_Render_Assets::make_url( 'admin', false ) );
+			\Caldera_Forms_Admin_Assets::set_cf_admin( \Caldera_Forms_Render_Assets::make_slug( 'pro' ) );
 		}
-		ob_start();
-		include $view_dir . '/index.php';
-		$str = ob_get_clean();
-		foreach ( [
-			'styles',
-			'manifest',
-			'vendor',
-			'client'
-		] as $thing ){
-			$str = str_replace( '/' . $thing, $this->get_assets_url() . $thing, $str );
+		\Caldera_Forms_Render_Assets::enqueue_style( 'pro' );
+		\Caldera_Forms_Render_Assets::enqueue_script( 'pro' );
+		wp_localize_script( \Caldera_Forms_Render_Assets::make_slug( 'pro' ), 'CF_PRO_ADMIN', $this->data() );
+		if( 'tab' === $context ){
+			$id = 'cf-pro-app-tab';
+		}else{
+			$id = 'cf-pro-tab';
 		}
-
-		if ( $context ) {
-			$str = str_replace( 'cf-pro-app', 'cf-pro-app-' . $context, $str );
-		}
-
-		return $inline .str_replace([
-				'<head>',
-				'</head>'
-			], '', $str );
+		return  sprintf('<div id="%s"></div>', $id );
 	}
 
 	/**
@@ -72,14 +68,14 @@ class scripts {
 	 */
 	public function data(){
 
-	    $pro_url = admin_url('admin.php?page=cf-pro');
+		$pro_url = admin_url('admin.php?page=cf-pro');
 
 		$data = array(
 			'strings' =>  [
 				'saved' => esc_html__( 'Settings Saved', 'caldera-forms' ),
 				'notSaved' => esc_html__( 'Settings could not be saved', 'caldera-forms' ),
-		                'apiKeysViewText' => esc_html__( 'You must add your API keys to use Caldera Forms Pro', 'caldera-forms' ),
-		                'apiKeysViewLink' => esc_url( $pro_url ),
+				'apiKeysViewText' => esc_html__( 'You must add your API keys to use Caldera Forms Pro', 'caldera-forms' ),
+				'apiKeysViewLink' => esc_url( $pro_url ),
 				'minLogLevelTitle' => esc_html__( 'Minimum Log Level', 'caldera-forms' ),
 				'minLogLevelInfo' => esc_html__( 'Setting a higher level than notice may affect performance, and should only be used when instructed by support.', 'caldera-forms' ),
 			],
