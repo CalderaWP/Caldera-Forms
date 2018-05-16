f18af6cb2cd3039d98b181b09cacb55326dee4b6
Nicolas Figueira <nico.figueira.mahe@gmail.com>
2018-05-16 23:20:32 +0200

    WIP Change tabs of Pro UI for non connected users
    #2391 Chabged the tabs
    *Needs translatable texts from string getter and links

---
+++ clients/pro/components/NotConnected/freeTrial.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec dapibus felis, vitae malesuada libero. Integer rutrum sodales nulla, eget fermentum ligula faucibus in. Nulla efficitur ac urna sit amet fringilla.</p>
+        <div>
+            <a class="cf-button-linked" v-on:click="clickedFreeTrial">View Documentation</a>
+            <a class="cf-button-linked" v-on:click="clickedDocumentation">Start Free Trial</a>
+        </div>
+    </div>
+</template>
+
+<script>
+  import { mapGetters,mapMutations } from 'vuex'
+
+  export default {
+    name: 'freeTrial',
+    method: {
+      clickedFreeTrial(){
+        this.$store.dispatch('freeTrialLink');
+      },
+      clickedDocumentation(){
+        this.$store.dispatch('documentationLink');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .paragraph {
+        max-width: 40rem;
+    }
+    .cf-button-linked {
+        padding: 1rem 1.4rem;
+        background-color: #ff7e30;
+        color: white;
+        cursor: pointer;
+        display: inline-block;
+    }
+</style>
---
+++ clients/pro/components/NotConnected/whatIsCalderaFormsPro.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec dapibus felis, vitae malesuada libero. Integer rutrum sodales nulla, eget fermentum ligula faucibus in. Nulla efficitur ac urna sit amet fringilla.</p>
+        <h3>Benefits</h3>
+        <ul class="list">
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>consectetur adipiscing elit.</li>
+            <li>consectetur adipiscing elit.</li>
+            <li>consectetur adipiscing elit.</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'whatIsCalderaFormsPro'
+  }
+</script>
+
+<style scoped>
+    .list {
+        list-style-type: disc;
+        padding-left: 2rem;
+    }
+</style>
