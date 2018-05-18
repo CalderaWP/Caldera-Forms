88894525788c5c33ab59290639247f05467af7e8
Josh Pollock <josh@calderawp.com>
2018-05-17 21:45:16 -0400

    #2391 update pro not connected strings

--- clients/pro/components/NotConnected/freeTrial.vue
+++ clients/pro/components/NotConnected/freeTrial.vue
@@ -2,8 +2,18 @@
     <div>
         <p class="paragraph">{{ strings.freeTrial.firstParagraph }}</p>
         <div>
-            <a class="cf-button-linked" href="https://calderaforms.com/caldera-forms-documentation-2/" target="_blank">{{ strings.freeTrial.buttonLeft }}</a>
-            <a class="cf-button-linked" href="https://calderaforms.com/pro/#buy" target="_blank">{{ strings.freeTrial.buttonRight }}</a>
+            <a
+                class="cf-button-linked"
+                href="https://calderaforms.com/doc/caldera-forms-pro-getting-started?utm_source=wp-admin&utm_campaign=pro-screen&utm_term=not-connected"
+                target="_blank">
+                {{ strings.freeTrial.buttonLeft }}
+            </a>
+            <a
+                class="cf-button-linked"
+                href="https://calderaforms.com/checkout?edd_action=add_to_cart&download_id=64101&edd_options[price_id]=1?utm_source=wp-admin&utm_campaign=pro-screen&utm_term=not-connected"
+                target="_blank">
+                {{ strings.freeTrial.buttonRight }}
+            </a>
         </div>
     </div>
 </template>
