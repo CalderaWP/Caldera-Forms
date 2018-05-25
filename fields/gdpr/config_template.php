8ae34bf55fbfbb86652a4053464e53da758ab3c2
Josh Pollock <josh@calderawp.com>
2018-05-25 13:53:56 -0400

    #2428 make an agreement setting in consent field and update markup

--- fields/gdpr/config_template.php
+++ fields/gdpr/config_template.php
@@ -8,22 +8,46 @@
 </div>
 
 <div class="caldera-config-group">
+	<label for="{{_id}}_agreement">
+        <?php esc_html_e( 'Agreement', 'caldera-forms' ); ?>
+	</label>
+	<div class="caldera-config-field">
+        <textarea
+				data-id="{{_id}}"
+				id="{{_id}}_agreement"
+				class="block-input field-config required"
+				name="{{_name}}[agreement]"
+				aria-describedby="{{_id}}_agreement-description"
+				required
+		>{{agreement}}</textarea><?php //keep on same line to avoid saving a tab in content ?>
+		<p
+				class="description"
+				id="{{_id}}_agreement-description"
+		>
+            <?php esc_html_e( 'The terms of the agreement to data collection.', 'caldera-forms' ); ?>
+		</p>
+	</div>
+</div>
+
+<div class="caldera-config-group">
     <label for="{{_id}}_linked_text">
         <?php esc_html_e( 'Linked Text', 'caldera-forms' ); ?>
     </label>
     <div class="caldera-config-field">
-        <textarea
+        <input
+				required
 				data-id="{{_id}}"
 				id="{{_id}}_linked_text"
-				class="block-input field-config"
+				class="block-input field-config required"
 				name="{{_name}}[linked_text]"
 				aria-describedby="{{_id}}_linked_text-description"
-		>{{linked_text}}</textarea><?php //keep on same line to avoid saving a tab in content ?>
+				value="{{linked_text}}"
+		/>
 		<p
 				class="description"
 				id="{{_id}}_linked_text-description"
 		>
-            <?php esc_html_e( 'This text will be linked to Privacy Policy content page. Explain what data will', 'caldera-forms' ); ?>
+            <?php esc_html_e( 'This text will be linked to Privacy Policy content page.', 'caldera-forms' ); ?>
         </p>
     </div>
 </div>
