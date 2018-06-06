5d62790f7345501edefe72288056472d2b761d1b
Josh Pollock <josh@calderawp.com>
2018-06-06 17:09:09 -0400

    #2514 compile change in last commit.

--- assets/js/entry-viewer-2.js
+++ assets/js/entry-viewer-2.js
@@ -1,4 +1,4 @@
-/*! GENERATED SOURCE FILE caldera-forms - v1.7.0 - 2018-05-31 *//**
+/*! GENERATED SOURCE FILE caldera-forms - v1.7.0 - 2018-06-06 *//**
  * API Client for Caldera Forms API for a single form
  *
  * @since 1.5.0
