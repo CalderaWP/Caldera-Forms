4f0bc439dfab293b43a3faf10162801331fc37c7
Josh Pollock <josh@calderawp.com>
2018-06-26 15:15:29 -0400

    #2598 show doc links in empty page body of privacy screen before loading form

--- clients/privacy/components/DocLinks.js
+++ clients/privacy/components/DocLinks.js
@@ -1,34 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Twemoji } from 'react-emoji-render';
-import {Panel, ListGroup, ListGroupItem } from 'react-bootstrap'
-import {DocLinks} from "./DocLinks";
-export const HelpBox = (props) => {
+import {ListGroup, ListGroupItem } from 'react-bootstrap'
+export const DocLinks = () => {
     return (
-        <div
-            style={{
-                backgroundColor: '#fff'
-            }}
-            className={'col-xs-4'}
-        >
-            <p
-                style={{
-                    margin: 0,
-                    padding: '.7em 1em',
-                    borderBottom: '1px solid #eee'
-                }}
-            >
-                {props.saveButton}
-            </p>
-            <h3>
-                <Twemoji text=":volcano: FAQ" />
-            </h3>
-           <DocLinks />
-
-
-            <h3>
-                <Twemoji text=":volcano: Documentation" />
-            </h3>
+        <React.Fragment>
             <ListGroup
                 style={{padding:'12px'}}
             >
@@ -45,10 +20,7 @@ export const HelpBox = (props) => {
                     </a>
                 </ListGroupItem>
             </ListGroup>
-        </div>
+        </React.Fragment>
     )
 };
 
-HelpBox.propTypes = {
-    saveButton: PropTypes.object.isRequired
-};
