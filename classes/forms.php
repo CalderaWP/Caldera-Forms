209c8eeb83ad88305e86a690992b94ae3d115172
Josh Pollock <josh@calderawp.com>
2018-10-18 12:32:19 -0400

    Minor code clean up for #2768

--- classes/forms.php
+++ classes/forms.php
@@ -570,11 +570,12 @@ class Caldera_Forms_Forms {
 		// get form templates (PROBABLY NEED TO MOVE METHOD INTO THIS CLASS)
 		$form_templates = Caldera_Forms_Admin::internal_form_templates();
 
-        $original_function_args = $newform;
+        	$original_function_args = $newform;
 
 		if(!empty($newform['clone'])){
 			$clone = $newform['clone'];
 		}
+		
 		// load template if any
 		if( !empty( $newform['template'] ) ){
 			if( isset( $form_templates[ $newform['template'] ] ) && !empty( $form_templates[ $newform['template'] ]['template'] ) ){
@@ -617,7 +618,6 @@ class Caldera_Forms_Forms {
 		 * @param array $newform New form config
 		 */
 		$newform = apply_filters( 'caldera_forms_create_form', $newform);
-
 
 		self::update_registry( $id );
 
