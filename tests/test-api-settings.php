ff6a951b992acb2d2ce701df52084a639ba8a2bb
New0 <nico.figueira.mahe@gmail.com>
2018-12-04 12:44:53 +0100

    Tests for settings route This test the responses of api requests @shelob9 I couldn't test post requests using args, right now that broke the tests

--- tests/test-api-settings.php
+++ tests/test-api-settings.php
@@ -9,6 +9,22 @@ class TestApiSettings extends CF_Rest_Test_Case
 	 */
 	protected $route_name = 'settings';
 
+
+	/**
+	 * Test un-authenticated call
+	 *
+	 * @since 1.8.0
+	 *
+	 * @covers 'cf-api/v2/settings' API route
+	 */
+	public function test_unauth_get_settings_response() {
+
+		$request = new WP_REST_Request( 'GET', $this->namespaced_route );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 401, $response->get_status() );
+
+	}
+
 	/**
 	 * Test GET settings route response
 	 *
@@ -17,10 +33,20 @@ class TestApiSettings extends CF_Rest_Test_Case
 	 * @covers 'cf-api/v2/settings' API route
 	 */
 	public function test_get_settings_response() {
-		$request  = new WP_REST_Request( 'GET', $this->namespaced_route );
+
+		wp_set_current_user( '1' );
+		$request = new WP_REST_Request( 'GET', $this->namespaced_route );
 		$response = $this->server->dispatch( $request );
-		$this->assertEquals( 200, $response->get_status() );
+		$this->assertEquals( 201, $response->get_status() );
+
+		//Test Data
 		$data = $response->get_data();
+		$this->assertArrayHasKey('styleIncludes', $data);
+		$this->assertArrayHasKey('cdnEnable', $data);
+		$this->assertArrayHasKey('grid', $data['styleIncludes']);
+		$this->assertArrayHasKey('alert', $data['styleIncludes']);
+		$this->assertArrayHasKey('form', $data['styleIncludes']);
+
 	}
 
 	/**
@@ -31,24 +57,38 @@ class TestApiSettings extends CF_Rest_Test_Case
 	 * @covers 'cf-api/v2/settings' API route
 	 */
 	public function test_post_settings_response() {
+
+		// Set current user
+		wp_set_current_user( '1' );
 		$request  = new WP_REST_Request( 'POST', $this->namespaced_route );
 		$response = $this->server->dispatch( $request );
-		$this->assertEquals( 200, $response->get_status() );
+		$this->assertEquals( 201, $response->get_status() );
 		$data = $response->get_data();
+		$this->assertArrayHasKey('styleIncludes', $data);
+		$this->assertArrayHasKey('cdnEnable', $data);
+		$this->assertArrayHasKey('grid', $data['styleIncludes']);
+		$this->assertArrayHasKey('alert', $data['styleIncludes']);
+		$this->assertArrayHasKey('form', $data['styleIncludes']);
+
 	}
 
 	/**
-	 * Test POST settings route response
+	 * Test POST settings/entries route response
 	 *
 	 * @since 1.8.0
 	 *
 	 * @covers 'cf-api/v2/settings/entries' API route
 	 */
 	public function test_post_settings_entries_response() {
-		$request  = new WP_REST_Request( 'POST', $this->namespaced_route . 'entries');
+
+		// Set current user
+		wp_set_current_user( '1' );
+		$request  = new WP_REST_Request( 'POST', $this->namespaced_route . '/entries');
 		$response = $this->server->dispatch( $request );
 		$this->assertEquals( 200, $response->get_status() );
 		$data = $response->get_data();
+		$this->assertArrayHasKey('per_page', $data);
+
 	}
 
 
