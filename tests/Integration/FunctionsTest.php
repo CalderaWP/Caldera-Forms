b92299637cc78db9039c61d801826384a8d3429a
Josh Pollock <josh@calderawp.com>
2018-12-14 18:19:22 -0500

    Add test for upgrade of file field type #2830

--- tests/Integration/FunctionsTest.php
+++ tests/Integration/FunctionsTest.php
@@ -38,4 +38,28 @@ class FunctionsTest extends TestCase
 		$container = caldera_forms_get_v2_container();
 		$this->assertTrue( is_object( $container->getService(  \calderawp\calderaforms\cf2\Services\QueueService::class)));
 	}
+
+	/**
+	 * @since  1.8.0
+	 *
+	 * @covers \caldera_forms_get_v2_container()
+	 */
+	public function testUpgradesAdvancedFileFields(){
+		$id = 'fld1';
+		$fieldConfig = [
+			'id' => $id,
+			'slug' => 'roy',
+			'type' => 'advanced_file',
+			'conditions' => [
+				'type' => 'IS'
+			]
+		];
+		$field = \Caldera_Forms_Field_Util::get_field($id,[
+			'fields' => [
+				$id => $fieldConfig
+			],
+			'conditional_groups' => []
+		], true);
+		$this->assertSame(\calderawp\calderaforms\cf2\Fields\FieldTypes\FileFieldType::getCf1Identifier(), $field['type'] );
+	}
 }
