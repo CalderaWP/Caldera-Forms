2b969cf16822d8ceebf706de3ee5c81ac021b240
New0 <nico.figueira.mahe@gmail.com>
2018-12-21 11:53:14 +0100

    Add fileUploads.js to render ( not attached to previous commit) #2818

---
+++ clients/render/fileUploads.js
@@ -0,0 +1,31 @@
+import {removeFromBlocking, removeFromPending, removeFromUploadStarted} from "./util";
+
+export const handleFileUploadResponse = (response,cf2,$form,messages,field) => {
+
+	const {fieldId} = field;
+	if( 'object' !== typeof  response ){
+		removeFromUploadStarted(fieldId,cf2);
+		removeFromPending(fieldId,cf2);
+		throw 'Upload Error';
+	}
+	else if (response.hasOwnProperty('control')) {
+		removeFromPending(fieldId,cf2);
+		removeFromBlocking(fieldId,cf2);
+		cf2.uploadCompleted.push(fieldId);
+		$form.submit();
+	}else{
+		if( response.hasOwnProperty('message') ){
+			messages[field.fieldIdAttr] = {
+				error: true,
+				message: response.hasOwnProperty('message') ? response.message : 'Invalid'
+			};
+		}
+		removeFromUploadStarted(fieldId,cf2);
+		removeFromPending(fieldId,cf2);
+		throw response;
+	}
+};
+
+export  const handleFileUploadError = (error) => {
+
+};
