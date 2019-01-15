0fa5d7e9d1b5dd310fb62057c961b50f08fce448
Nicolas Figueira <nico.figueira.mahe@gmail.com>
2019-01-15 17:30:50 +0100

    Revert "Refactor index.js to have testable functions (#2854)"
    This reverts commit 0ef623e59408bbd50b08275478452dfa73bccb7c.

--- clients/tests/setupJest.js
+++ clients/tests/setupJest.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import jquery from 'jquery';
 
 global.wp = {
 	shortcode: {
@@ -13,14 +12,3 @@ global.wp = {
 Object.defineProperty( global.wp, 'element', {
 	get: () => React,
 } );
-
-/**
- * Setup fetch mocking
- * @link https://www.npmjs.com/package/jest-fetch-mock#usage
- */
-global.fetch = require('jest-fetch-mock');
-
-/**
- * Setup jQuery
- */
-global.$ = global.jQuery = jquery;
