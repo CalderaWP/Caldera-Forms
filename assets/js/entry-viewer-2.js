d89c1a2049eaef467e703a1dfea485ee116a0ba7
New0 <nico.figueira.mahe@gmail.com>
2019-01-15 09:11:53 +0100

    Update branch with develop

--- assets/js/entry-viewer-2.js
+++ assets/js/entry-viewer-2.js
@@ -1,4 +1,4 @@
-/*! GENERATED SOURCE FILE caldera-forms - v1.8.0-alpha.1 - 2019-01-14 *//**
+/*! GENERATED SOURCE FILE caldera-forms - v1.8.0-alpha.1 - 2019-01-15 *//**
  * API Client for Caldera Forms API for a single form
  *
  * @since 1.5.0
