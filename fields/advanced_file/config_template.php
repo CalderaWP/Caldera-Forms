b56c3e09b8020d2f2c0e44a9e4268b6fae8510ac
Josh Pollock <josh@calderawp.com>
2019-01-22 12:10:11 -0500

    #2925 fix discontinued message

--- fields/advanced_file/config_template.php
+++ fields/advanced_file/config_template.php
@@ -1,6 +1,6 @@
 <?php
 printf( '<div class="error"><p>%1s</p></div>',
-	__( 'This file type is discontinued and no longer supported. Please switch to the new advanced file field', 'caldera-forms' )
+	__( 'This file type is discontinued and no longer supported. Please switch to the new advanced file field.', 'caldera-forms' )
 );
 ?>
 <div class="caldera-config-group">
