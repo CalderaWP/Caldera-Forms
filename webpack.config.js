1a6e70e801076fd530f37c53060ab958f6d8cb54
Josh Pollock <josh@calderawp.com>
2019-03-21 15:39:20 -0400

    exclude react and react dom from webpack builds #3065

--- webpack.config.js
+++ webpack.config.js
@@ -51,8 +51,11 @@ const entryPointNames = [
 
 // Setup externals
 const externals = {
-    jquery: 'jQuery'
+	'jquery': 'jQuery',
+	'react': 'React',
+	'react-dom': 'ReactDOM'
 };
+
 // Setup external for each entry point
 entryPointNames.forEach( entryPointName => {
     externals[ '@/calderaForms' + entryPointName ] = {
