f4aa624d12a326e2e1eca1dc5435e24f407de74f
New0 <nico.figueira.mahe@gmail.com>
2019-03-29 15:17:51 +0100

    Package changes

--- assets/js/caldera-forms-front.js
+++ assets/js/caldera-forms-front.js
@@ -1,4 +1,4 @@
-/*! GENERATED SOURCE FILE caldera-forms - v1.8.3 - 2019-03-26 */var resBaldrickTriggers;
+/*! GENERATED SOURCE FILE caldera-forms - v1.8.4 - 2019-03-29 */var resBaldrickTriggers;
 
 jQuery(function($){
 	function fieldErrors(fields, $form, $notice) {
