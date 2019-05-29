734149d51ca41b60a84460cb6192d909202ada57
Josh Pollock <josh@calderawp.com>
2019-05-29 18:35:50 -0400

    Update deleteBuilds.js
    Inline docs for delete builds #3239

--- clients/deleteBuilds.js
+++ clients/deleteBuilds.js
@@ -1,3 +1,11 @@
+/**
+ * Delete everything in webpack client build dirs
+ *
+ * @since 1.8.6
+ *
+ * @type {module:fs}
+ */
+
 const fs = require('fs');
 const path = require( 'path' );
 [
@@ -9,7 +17,7 @@ const path = require( 'path' );
     fs.readdir(directory, (err, files) => {
         if (err) throw err;
         for (const file of files) {
-            console.log(path.join(directory, file));
+            console.log(`Deleting ${path.join(directory, file)}`);
             fs.unlink(path.join(directory, file), err => {
                 if (err) throw err;
             });
