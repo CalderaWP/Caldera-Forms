f8c339096ed1908c6578cb468e1fdf6e34fbbf63
Nico Figueira <nico.figueira.mahe@gmail.com>
2019-10-31 13:19:38 +0100

    Remove inline styles

--- clients/components/SendWP/GetSendWP/GetSendWP.js
+++ clients/components/SendWP/GetSendWP/GetSendWP.js
@@ -12,10 +12,9 @@ export const GetSendWP = (props) => {
 
 
 	return(
-		<div className={classNames(props.className,GetSendWP.classNames.wrapper)} style={{margin:'20px'}}>
-			<div className="caldera-forms-clippy-zone-inner-wrap" style={{background: 'white'}}>
-				<div className="caldera-forms-clippy"
-					style={{textAlign:'center',backgroundColor:'white',padding:'20px'}}>
+		<div className={classNames(props.className,GetSendWP.classNames.wrapper)}>
+			<div className="caldera-forms-clippy-zone-inner-wrap">
+				<div className="caldera-forms-clippy">
 					<h2>
 						{__( 'Getting WordPress email into an inbox just got a lot easier!', 'caldera-forms' )}
 					</h2>
@@ -26,7 +25,7 @@ export const GetSendWP = (props) => {
 						)}
 					</p>
 					<a href="https://sendwp.com?utm_source=Caldera+Forms+Plugin&utm_medium=Forms_Email+Settings&utm_campaign=SendWP+banner+ad"
-						target="_blank" className="bt-btn btn btn-green" style={{width: '80%',margin: 'auto', paddingTop: '12px' }}>
+						target="_blank" className="bt-btn btn btn-green" >
 						{__( 'Learn More', 'caldera-forms' )}
 					</a>
 				</div>
@@ -40,5 +39,5 @@ export const GetSendWP = (props) => {
  * @type {{wrapper: string}}
  */
 GetSendWP.classNames = {
-	wrapper: 'caldera-forms-global-form-settings'
+	wrapper: 'caldera-forms-global-form-settings caldera-forms-global-getsendwp-wrapper'
 };
