185631a8e938a8a625b1aa188d6c796025fa7a1c
New0 <nico.figueira.mahe@gmail.com>
2019-12-09 14:58:41 +0100

    Remove Pro Tab from Form edition page

--- includes/cf-pro-client/bootstrap-cf-pro.php
+++ includes/cf-pro-client/bootstrap-cf-pro.php
@@ -31,12 +31,15 @@ add_action('caldera_forms_includes_complete', function () {
 		$view_dir = __DIR__ . '/dist';
 		$scripts = new scripts($assets_url, $slug, CF_PRO_VER);
 		if ( Caldera_Forms_Admin::is_edit() ) {
-			add_action('admin_init', function () use ($scripts, $view_dir) {
-				$tab = new \calderawp\calderaforms\pro\admin\tab(__DIR__ . '/dist/tab.php');
-				add_action('caldera_forms_get_panel_extensions', [ $tab, 'add_tab' ]);
-				container::get_instance()->set_tab_html($scripts->webpack($view_dir, 'tab', false));
+			// @see https://github.com/CalderaWP/Caldera-Forms/issues/3413
+			if( Caldera_Forms_Admin::show_pro_ui() ){
+				add_action('admin_init', function () use ($scripts, $view_dir) {
+					$tab = new \calderawp\calderaforms\pro\admin\tab(__DIR__ . '/dist/tab.php');
+					add_action('caldera_forms_get_panel_extensions', [ $tab, 'add_tab' ]);
+					container::get_instance()->set_tab_html($scripts->webpack($view_dir, 'tab', false));
 
-			});
+				});
+			}
 		}
 		// @see https://github.com/CalderaWP/Caldera-Forms/issues/3413
 		if( Caldera_Forms_Admin::show_pro_ui() ){
