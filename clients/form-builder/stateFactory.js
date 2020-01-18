416ee582368a03ede01c4d378e0c3cf6244f940a
Josh Pollock <jpollock412@gmail.com>
2020-01-18 16:59:40 -0500

    update snapshots #3435

--- clients/form-builder/stateFactory.js
+++ clients/form-builder/stateFactory.js
@@ -11,10 +11,10 @@ import testForm from "../tests/unit/form-builder/test-form";
  */
 function prepareTags(tags) {
     const initialValues = [];
-     Object.keys(tags).forEach(tagType => {
+    Object.keys(tags).forEach(tagType => {
         const tagsOfType = tags[tagType];
 
-        if( tagsOfType.length ){
+        if (tagsOfType.length) {
             tagsOfType.forEach(tag => {
                 initialValues.push({type: tagType, tag})
             });
@@ -32,14 +32,15 @@ function prepareTags(tags) {
  * @param currentFormFields
  * @returns {Array}
  */
-function prepareFields(currentFormFields){
+function prepareFields(currentFormFields) {
     return currentFormFields && Object.keys(currentFormFields).length ? Object.keys(currentFormFields).map(
         id => {
             const field = currentFormFields[id];
-            return {...field,ID:id,tag: '%' + field.slug + '%'}
+            return {...field, ID: id, tag: '%' + field.slug + '%'}
         }
     ) : [];
 }
+
 /**
  * Utility function for adding magic types from default system_tags array.
  *
@@ -49,14 +50,14 @@ function prepareFields(currentFormFields){
  * @param state
  * @returns {[]}
  */
-function addInitialMagicTypes(system_values,state) {
+function addInitialMagicTypes(system_values, state) {
     const other = [];
     Object.keys(system_values).forEach(magicType => {
-        if(undefined !== magicType && ! ['field', 'system'].includes(magicType)){
-            other.push( system_values[magicType] );
+        if (undefined !== magicType && !['field', 'system'].includes(magicType)) {
+            other.push(system_values[magicType]);
         }
     });
-    if( other && other.length ){
+    if (other && other.length) {
         other.forEach(o => state.addMagicType(
             o.type, true, prepareTags(o.tags)
         ));
@@ -64,7 +65,6 @@ function addInitialMagicTypes(system_values,state) {
     return other;
 
 }
-
 
 
 /**
@@ -76,22 +76,22 @@ function addInitialMagicTypes(system_values,state) {
  * @returns {*}
  */
 export const getAllFieldsUsed = (state) => {
-    const groups =  state.getAllConditionals();
-    if( ! groups.length ){
+    const groups = state.getAllConditionals();
+    if (!groups.length) {
         return [];
     }
 
     let fields = [];
-     groups.forEach( ( conditional) => {
-        if( conditional.hasOwnProperty('config') && conditional.config.hasOwnProperty('fields') ){
-            const _fields =  Object.values(conditional.config.fields);
-            if( _fields.length ){
-                _fields.map( _f => fields.push(_f));
+    groups.forEach((conditional) => {
+        if (conditional.hasOwnProperty('config') && conditional.config.hasOwnProperty('fields')) {
+            const _fields = Object.values(conditional.config.fields);
+            if (_fields.length) {
+                _fields.map(_f => fields.push(_f));
             }
         }
 
     });
-     return  fields;
+    return fields;
 };
 
 /**
@@ -102,16 +102,16 @@ export const getAllFieldsUsed = (state) => {
  * @param conditionalId
  * @param {cfEditorState }state
  */
-export const getFieldsUsedByConditional = (conditionalId,state) => {
+export const getFieldsUsedByConditional = (conditionalId, state) => {
     const conditional = state.getConditional(conditionalId);
-    if( ! conditional ){
+    if (!conditional) {
         return [];
     }
 
-    if( ! conditional.hasOwnProperty('config') || !  conditional.config.hasOwnProperty('fields') ) {
+    if (!conditional.hasOwnProperty('config') || !conditional.config.hasOwnProperty('fields')) {
         return [];
     }
-    return  Object.values(conditional.config.fields);
+    return Object.values(conditional.config.fields);
 };
 
 /**
@@ -122,19 +122,19 @@ export const getFieldsUsedByConditional = (conditionalId,state) => {
  * @param conditionalId
  * @param {cfEditorState }state
  */
-export const getFieldsNotAllowedForConditional = (conditionalId,state) => {
+export const getFieldsNotAllowedForConditional = (conditionalId, state) => {
     const allFieldsUsed = getAllFieldsUsed(state);
     //No fields used by ANY conditional? If so, all fields can be used.
-    if( ! allFieldsUsed.length ){
+    if (!allFieldsUsed.length) {
         return [];
     }
-    const fieldsUsedByConditional = getFieldsUsedByConditional(conditionalId,state);
+    const fieldsUsedByConditional = getFieldsUsedByConditional(conditionalId, state);
     //No fields used by THIS conditional? If so, return allFieldsUsed un filtered
-    if( ! fieldsUsedByConditional.length ){
-       // return allFieldsUsed;
+    if (!fieldsUsedByConditional.length) {
+        // return allFieldsUsed;
     }
     //Filter out fields used by OTHER conditional groups
-    return allFieldsUsed.filter( fieldId => !fieldsUsedByConditional.includes(fieldId) );
+    return allFieldsUsed.filter(fieldId => !fieldsUsedByConditional.includes(fieldId));
 
 };
 
@@ -146,13 +146,13 @@ export const getFieldsNotAllowedForConditional = (conditionalId,state) => {
  * @param conditionalGroup
  * @returns {{id: *, type: (*|string), config: {fields: (*|[]), group: *}}}
  */
-const conditionalFromCfConfig =  (conditionalGroup) =>{
-    return  {
+const conditionalFromCfConfig = (conditionalGroup) => {
+    return {
         id: conditionalGroup.id,
         type: conditionalGroup.type ? conditionalGroup.type : 'show',
         config: {
-            name: conditionalGroup.hasOwnProperty('name'  ) ? conditionalGroup.name : '',
-            fields: conditionalGroup.hasOwnProperty('fields' ) ? Object.values(conditionalGroup.fields) : [],
+            name: conditionalGroup.hasOwnProperty('name') ? conditionalGroup.name : '',
+            fields: conditionalGroup.hasOwnProperty('fields') ? Object.values(conditionalGroup.fields) : [],
             group: conditionalGroup.hasOwnProperty('group') ? conditionalGroup.group : {},
         }
     };
@@ -165,8 +165,8 @@ const conditionalFromCfConfig =  (conditionalGroup) =>{
  * @param conditions
  * @param state
  */
-export const setConditionalsFromCfConfig = (conditions,state) => {
-     Object.values(conditions).map(group => state.addConditional(conditionalFromCfConfig(group)));
+export const setConditionalsFromCfConfig = (conditions, state) => {
+    Object.values(conditions).map(group => state.addConditional(conditionalFromCfConfig(group)));
 };
 
 /**
@@ -178,18 +178,18 @@ export const setConditionalsFromCfConfig = (conditions,state) => {
  * @param current_form_fields
  * @returns {{prepareSystemTags: (function(): []), createState: (function(): {getAllMagicTags: () => magicTags; getMagicTagsByType: (type: string) => magicTags; addMagicType: (typeName: string, brackets: boolean, intialTags?: (typeAndTags | undefined)) => void; removeMagicType: (typeName: string) => boolean; getAllSystemValues(): systemValues; feildEvents: EventEmitter<{addField: (fieldId: string) => void; updateField: (args: {fieldId: string; beforeUpdate: field}) => void; removeField: (field: field) => void}>; addField(field: field): boolean; getField(fieldId: string): (field | undefined); updateField(field: field): boolean; getAllFields(): fields; removeField(fieldId: string): boolean; addConditional(conditional: conditional): boolean; removeConditional(conditionalId: string): boolean; updateConditional(conditional: conditional): boolean; getConditional(conditionalId: string): (conditional | undefined); getAllConditionals(): conditionals})}}
  */
-export default function (system_values,current_form_fields) {
+export default function (system_values, current_form_fields) {
     const systemTags = system_values && system_values.system.tags ? system_values.system.tags : [];
     const initialFields = prepareFields(current_form_fields);
-    const api =  {
+    const api = {
         prepareFields,
         createState: () => {
             const state = cfEditorState({
                 initialSystemValues: prepareTags(systemTags),
                 initialFields
             });
-            addInitialMagicTypes(system_values,state);
-            return  state;
+            addInitialMagicTypes(system_values, state);
+            return state;
 
         },
         prepareSystemTags: () => prepareTags(systemTags),
